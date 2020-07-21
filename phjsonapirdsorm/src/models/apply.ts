78ce823c55cdcb19205f20e30f49d9d1c1ebecf9
Alex <pqian@pharbers.com>
2020-07-21 17:13:52 +0800

    apply user添加接口增加完成，bug修复完毕

---
+++ phjsonapirdsorm/src/models/apply.ts
@@ -0,0 +1,14 @@
+const records = {
+    apply: {
+        company: String,
+        department: String,
+        email: String,
+        intention: String,
+        name: String,
+        phone: String,
+        position: String,
+        create: Date
+    }
+}
+
+export default records
--- phjsonapirdsorm/tests/unit/test-handler.js
+++ phjsonapirdsorm/tests/unit/test-handler.js
@@ -2,7 +2,7 @@
 
 const app = require('../../app.js')
 const delegate = require("../../dist/delegate/appLambdaDelegate").default
-const phlogger = require("../../dist/logger/phLogger").default
+const phLogger = require("../../dist/logger/phLogger").default
 const chai = require('chai')
 const expect = chai.expect
 const fs = require('fs')
@@ -10,22 +10,38 @@ var context;
 // const mongoose = require("mongoose")
 
 describe('Tests index', function () {
-	it('verify find one successfully', async () => {
-	    const event = JSON.parse(fs.readFileSync("../events/event_success_find_one.json", 'utf8'))
+
+	it('verify post one', async () => {
+	    const event = JSON.parse(fs.readFileSync("../events/event_success_apply_post_one.json", 'utf8'))
 	    const result = await app.lambdaHandler(event, context)
 
 	    expect(result).to.be.an('object');
-	    expect(result.statusCode).to.equal(200);
+	    expect(result.statusCode).to.equal(201);
 	    expect(result.body).to.be.an('string');
 
 	    let response = JSON.parse(result.body);
-	    phlogger.info(response)
 
 	    expect(response).to.be.an('object');
-	    expect(response.data.id).to.be.equal("n5DzBBvCCuVANODXHbfm");
-	    expect(response.data.type).to.be.equal("images");
-	    // expect(response.location).to.be.an("string");
+	    expect(response.data.type).to.be.equal("applies");
+
 	});
+	
+	// it('verify find one successfully', async () => {
+	//     const event = JSON.parse(fs.readFileSync("../events/event_success_find_one.json", 'utf8'))
+	//     const result = await app.lambdaHandler(event, context)
+	//
+	//     expect(result).to.be.an('object');
+	//     expect(result.statusCode).to.equal(200);
+	//     expect(result.body).to.be.an('string');
+	//
+	//     let response = JSON.parse(result.body);
+	//     phLogger.info(response)
+	//
+	//     expect(response).to.be.an('object');
+	//     expect(response.data.id).to.be.equal("n5DzBBvCCuVANODXHbfm");
+	//     expect(response.data.type).to.be.equal("images");
+	//     // expect(response.location).to.be.an("string");
+	// });
 
 	// it('verify find one error', async () => {
 	//     const event = JSON.parse(fs.readFileSync("../events/event_error_find_one.json", 'utf8'))
@@ -43,21 +59,21 @@ describe('Tests index', function () {
 	//     // expect(response.location).to.be.an("string");
 	// });
 	//
-	it('verify find relationship success', async () => {
-	    const event = JSON.parse(fs.readFileSync("../events/event_success_find_relationships.json", 'utf8'))
-	    const result = await app.lambdaHandler(event, context)
-
-		expect(result).to.be.an('object');
-	    expect(result.statusCode).to.equal(200);
-	    expect(result.body).to.be.an('string');
-
-	    let response = JSON.parse(result.body);
-
-	    expect(response).to.be.an('object');
-	    expect(response.data[0].type).to.be.equal("products");
-	    expect(response.data[0].id).to.be.equal("5e00862a28e9fe103c5e2f2d");
-	    // expect(response.location).to.be.an("string");
-	});
+	// it('verify find relationship success', async () => {
+	//     const event = JSON.parse(fs.readFileSync("../events/event_success_find_relationships.json", 'utf8'))
+	//     const result = await app.lambdaHandler(event, context)
+	//
+	// 	expect(result).to.be.an('object');
+	//     expect(result.statusCode).to.equal(200);
+	//     expect(result.body).to.be.an('string');
+	//
+	//     let response = JSON.parse(result.body);
+	//
+	//     expect(response).to.be.an('object');
+	//     expect(response.data[0].type).to.be.equal("products");
+	//     expect(response.data[0].id).to.be.equal("5e00862a28e9fe103c5e2f2d");
+	//     // expect(response.location).to.be.an("string");
+	// });
 
 	// it('verify find page success', async () => {
 	//     const event = JSON.parse(fs.readFileSync("../events/event_success_find_page.json", 'utf8'))
@@ -145,7 +161,7 @@ describe('Tests index', function () {
 	// 	expect(result.body).to.be.an('string');
 	//
 	// 	let response = JSON.parse(result.body);
-	// 	phlogger.info(response)
+	// 	phLogger.info(response)
 	//
 	// 	expect(response).to.be.an('object');
 	// 	expect(response.data.id).to.be.equal("n5DzBBvCCuVANODXHbfm");

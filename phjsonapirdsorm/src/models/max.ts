98d3a90450d7a6872ab4ffbe66efa98a90dd14c1
Alex <pqian@pharbers.com>
2020-08-17 16:58:52 +0800

    初始化max result reports Schema

---
+++ phjsonapirdsorm/src/models/max.ts
@@ -0,0 +1,25 @@
+
+const records = {
+    max: {
+       quarter: Number,
+       countryName: String,
+       provinceName: String,
+       cityName: String,
+       mkt: String,
+       moleName: String,
+       productName: String,
+       salesQty: Number,
+       salesValue: Number,
+       apex: String,
+       dimensionName: String,
+       dimensionValue: String,
+       year: Number,
+       month: Number,
+       company: String,
+       cuboidsId: Number,
+       cuboidsName: String,
+       lattles: Array(String)
+    }
+}
+
+export default records

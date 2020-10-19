390cf6b7e0a3eb441ab4f0531c41493d7acf6f5d
Alex <891366402@qq.com>
2020-10-19 23:12:16 +0800

    测试成功

--- phjsonapirdsorm/src/models/entry.ts
+++ phjsonapirdsorm/src/models/entry.ts
@@ -1,80 +1,104 @@
+class Entry {
+    public model: any = {
+        asset: {
+            name: String,
+            block: { link: "dataBlock", isArray: true, inverse: "assetBlock" },
+            owner: String,
+            extension: String,
+            size: Number,
+            source: String,
+            type: String, // candidate: database, file, stream, application, mart, cube
+            accessibility: String,
+            version: String,
+            isNewVersion: Boolean,
+            providers: Array(String),
+            markets: Array(String),
+            molecules: Array(String),
+            dateCover: Array(String),
+            geoCover: Array(String),
+            labels: Array(String),
+            created: Date,
+            modified: Date,
+            description: String,
+        },
+        dataBlock: {
+            assetBlock: {link: "asset", inverse: "block"},
+            dfs: {link: "dataSet", isArray: true, inverse: "blockDs"},
+            name: String,
+            label: String,
+            startRow: Number,
+            type: String,
+            description: String,
+        },
+        dataSet: {
+            parent: { link: "dataSet", isArray: true, inverse: "child" },
+            child: { link: "dataSet", isArray: true, inverse: "parent" },
+            blockDs: { link: "dataBlock", inverse: "dfs" },
+            sampleData: {link: "dataSetSample", isArray: true, inverse: "ds"},
+            job: { link: "job", inverse: "gen" },
+            name: String,
+            schema: Array(String),
+            source: String,
+            storeType: String,
+            size: Number,
+            created: Date,
+            modified: Date,
+            description: String,
+        },
+        dataSetSample: {
+            ds: {link: "dataSet", inverse: "sampleData"},
+            data: String,
+            created: Date,
+            modified: Date,
+            description: String,
+        },
+        job: {
+            gen: { link: "dataSet", isArray: true, inverse: "job" },
+            task: String,
+            inputDS: Array(String),
+            outPutDS: Array(String),
+            jobId: String,
+            asset: String,
+            status: String,
+            start: Date,
+            end: Date,
+            created: Date,
+            modified: Date,
+            description: String,
+        },
+        // dbSource: {
+        //     dbType: String,
+        //     url: String,
+        //     user: String,
+        //     pwd: String,
+        //     dbName: String,
+        //     create: Date,
+        //     assetDbs: { link: "asset", inverse: "dbs" }
+        // },
+    }
 
-const records = {
-    asset: {
-        name: String,
-        block: { link: "dataBlock", isArray: true, inverse: "assetBlock" },
-        owner: String,
-        extension: String,
-        size: Number,
-        source: String,
-        type: String, // candidate: database, file, stream, application, mart, cube
-        accessibility: String,
-        version: String,
-        isNewVersion: Boolean,
-        providers: Array(String),
-        markets: Array(String),
-        molecules: Array(String),
-        dateCover: Array(String),
-        geoCover: Array(String),
-        labels: Array(String),
-        created: Date,
-        modified: Date,
-        description: String,
-    },
-    dataBlock: {
-        assetBlock: {link: "asset", inverse: "block"},
-        dfs: {link: "dataSet", isArray: true, inverse: "blockDs"},
-        name: String,
-        label: String,
-        startRow: Number,
-        type: String,
-        description: String,
-    },
-    dataSet: {
-        parent: { link: "dataSet", isArray: true, inverse: "child" },
-        child: { link: "dataSet", isArray: true, inverse: "parent" },
-        blockDs: { link: "dataBlock", inverse: "dfs" },
-        sampleData: {link: "dataSetSample", isArray: true, inverse: "ds"},
-        job: { link: "job", inverse: "gen" },
-        name: String,
-        schema: Array(String),
-        source: String,
-        storeType: String,
-        size: Number,
-        created: Date,
-        modified: Date,
-        description: String,
-    },
-    dataSetSample: {
-        ds: {link: "dataSet", inverse: "sampleData"},
-        data: String,
-        created: Date,
-        modified: Date,
-        description: String,
-    },
-    job: {
-        gen: { link: "dataSet", isArray: true, inverse: "job" },
-        task: String,
-        inputDS: Array(String),
-        outPutDS: Array(String),
-        jobId: String,
-        asset: String,
-        status: String,
-        start: Date,
-        end: Date,
-        created: Date,
-        modified: Date,
-        description: String,
-    },
-    // dbSource: {
-    //     dbType: String,
-    //     url: String,
-    //     user: String,
-    //     pwd: String,
-    //     dbName: String,
-    //     create: Date,
-    //     assetDbs: { link: "asset", inverse: "dbs" }
-    // },
+    public operations = {
+        hooks: {
+            asset: [ this.hooksDate ],
+            dataSet: [ this.hooksDate ],
+            dataSetSample: [ this.hooksDate ],
+            job: [ this.hooksDate ]
+        }
+    }
+
+    protected hooksDate(context, record, update) {
+        const { request: { method, meta: { language } } } = context
+        switch (method) {
+            case "create":
+                const date = new Date()
+                record.created = date
+                record.modified = date
+                return record
+            case "update":
+                record.modified = new Date()
+                return record
+        }
+    }
 }
 
-export default records
+export default Entry
--- phjsonapirdsorm/tests/unit/adapter-entry-test.js
+++ phjsonapirdsorm/tests/unit/adapter-entry-test.js
@@ -1,17 +1,19 @@
 'use strict';
 
 const phLogger = require("phlayer").phLogger
-const dfs = require("phlayer").DBFactory
-
+const DBFactory = require("phlayer").DBFactory
+let pg;
 
 describe('convert data', function () {
     before('init', async () => {
-
+        pg = DBFactory.getInstance.getStore()
+        phLogger.info(pg)
     })
 
 
     it ('c oldAsset to newAsset', async () => {
-
+        const asset = await pg.find("asset")
+        phLogger.info(asset)
     })
 
     it ('c fileIndex to dataBlock', async () => {

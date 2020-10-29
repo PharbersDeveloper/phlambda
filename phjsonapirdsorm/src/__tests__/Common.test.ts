f19b05d5488a608dc41089115e39103c5bd391a6
Alex <891366402@qq.com>
2020-10-29 20:15:54 +0800

    更新基础库版本

--- phjsonapirdsorm/src/__tests__/Common.test.ts
+++ phjsonapirdsorm/src/__tests__/Common.test.ts
@@ -5,5 +5,5 @@ test("Update Account Password", async () => {
     const event = JSON.parse(fs.readFileSync("../events/event_common_patch_one.json", "utf8"))
     const app = require("../../app.js")
     const res = await app.lambdaHandler(event, undefined)
-    logger.info(JSON.parse(String(res.body)))
+    logger.info(JSON.parse(String(res.payload)))
 }, 5000)

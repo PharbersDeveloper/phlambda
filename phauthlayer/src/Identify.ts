e8e87835dee58265770d8fa079235739706a6c82
Alex <891366402@qq.com>
2020-12-02 14:31:32 +0800

    覆盖性单元测试 100% 结束

--- phauthlayer/src/Identify.ts
+++ phauthlayer/src/Identify.ts
@@ -21,7 +21,7 @@ export default class Identify implements IIdentify {
         this.httpMethod.set(permissions.A, ["get", "post", "patch", "delete"])
     }
 
-    verify(event: Map<string, any>, scope: string = "APP|phcommon:accounts:qtaGDePl1OrSFEgm:W,phcommon:parthers:psNeInomlGaSfgvd:R|W#APP|entry:assets&filter[parthers]=psNeInomlGaSfgvd:*:R,entry:assets&filter[owner]=qtaGDePl1OrSFEgm:*:W|W#APP|reports:parthers:5xeiSaYk_1noz-RKPyJ8:R,reports:templates:fVxL1xByKMkIAW1ct_su:R|R"): boolean {
+    verify(event: Map<string, any>, scope: string): boolean {
         if (!scope) return false
         const method = event["httpMethod"].toLowerCase()
         const paths = event["path"].split("/").filter((s: string) => s !== "")
@@ -29,17 +29,15 @@ export default class Identify implements IIdentify {
         // 如果是super admin scope = * 全权限 access
         if (splitScope.length === 1 && splitScope[0] === permissions.ALL) return true
 
-        splitScope = splitScope.filter(item => item.includes(paths[0]))
+        splitScope = splitScope.filter(item => item.includes(paths[0]) || item.includes("*"))
         const initScope = splitScope.shift().split("|")
 
         // 如果是平台全权限 admin scope = APP|*|权限，需验证operation是否符合权限
         if (initScope.length === 3 && initScope[1] === permissions.ALL
             && this.httpMethod.get(initScope[2].toLowerCase()).includes(method)) return true
 
-        // APP|phcommon:accounts:qtaGDePl1OrSFEgm:W,phcommon:parthers:psNeInomlGaSfgvd:R|W
-        // APP|entry:assets&filter[parthers]=psNeInomlGaSfgvd:*:R,entry:assets&filter[owner]=qtaGDePl1OrSFEgm:*:W|W
-
         const url = Object.keys(event["queryStringParameters"]).map((key: string) => {
+            // TODO 专门为前端不好的查询习惯兼容代码
             if ("ids[]" in event["multiValueQueryStringParameters"]
                 && event["multiValueQueryStringParameters"]["ids[]"].length === 1) {
                 event["pathParameters"] = Object.assign(event["pathParameters"], {id: event["multiValueQueryStringParameters"]["ids[]"][0]})
@@ -50,11 +48,11 @@ export default class Identify implements IIdentify {
         const detailARN = initScope[1].split(",")
         const flag = detailARN.map((arn: string) => {
             const detail = arn.split(":")
-            if ((detail[1] === permissions.ALL || detail[2] === permissions.ALL)
+            if ((detail[1] === permissions.ALL || (!detail[1].includes("&") && detail[2] === permissions.ALL))
                 && this.httpMethod.get(detail[3].toLowerCase()).includes(method)) {
                 return true
             }
-            if ("id" in event["pathParameters"]) {
+            if ("id" in event["pathParameters"] && !detail[1].includes("&")) {
                 return detail[1] === paths[1]
                     && event["pathParameters"].id === detail[2]
                     && this.httpMethod.get(detail[3].toLowerCase()).includes(method)

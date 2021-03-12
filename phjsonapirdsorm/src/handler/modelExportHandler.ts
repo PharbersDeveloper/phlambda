9df5266c4f6ca956460f2295b9b828fd245e7394
wodelu <1058744843@qq.com>
2021-03-12 15:06:40 +0800

    优化

--- phjsonapirdsorm/src/handler/modelExportHandler.ts
+++ phjsonapirdsorm/src/handler/modelExportHandler.ts
@@ -3,13 +3,10 @@ import {Http} from "../common/http"
 
 export async function modelExport(event: Map<string, any>) {
     // @ts-ignore
-    const body = JSON.parse(event.body)
-    const versionValue = body.version
-    const idValue = body.owner_id
+    const eventBody = JSON.parse(event.body)
     const { errors: { BadRequestError } } = fortune
     const airflowRunDagUrl = `http://192.168.112.226:30086/api/v1/dags/dw_export_table/dagRuns`
-    const parm = {version: versionValue, owner_id: idValue}
-    const res =  await new Http().post(airflowRunDagUrl, {conf: parm})
+    const res =  await new Http().post(airflowRunDagUrl, {conf: eventBody})
     if (res.status !== 200) { throw new BadRequestError(res.statusText) }
     return {
         headers: {},

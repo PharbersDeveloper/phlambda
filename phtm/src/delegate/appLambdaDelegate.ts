d86aad097983036caae4cc1f57766a9774272f5c
Alex <891366402@qq.com>
2021-03-21 01:18:44 +0800

    没错又是你鹏哥

--- phtm/src/delegate/appLambdaDelegate.ts
+++ phtm/src/delegate/appLambdaDelegate.ts
@@ -1,17 +1,24 @@
 // import { identify } from "phauthlayer"
-import { ConfigRegistered, JsonApiMain, Logger, PostgresConfig, RedisConfig, SF, Store } from "phnodelayer"
+import { ConfigRegistered, Logger, Main, PostgresConfig, RedisConfig, SF, Store } from "phnodelayer"
 import { PostgresqlConf } from "../common/config"
+import { callRHandler } from "../handler/callRHandler"
 
 export default class AppLambdaDelegate {
     async exec(event: Map<string, any>) {
+        // @ts-ignore
+        if (event.pathParameters.type === "callR") {
+            return await callRHandler(event)
+        }
         const postgresConf = new PostgresConfig(PostgresqlConf.entry, PostgresqlConf.user,
             PostgresqlConf.password, PostgresqlConf.url,
-            PostgresqlConf.port, PostgresqlConf.db)
+            PostgresqlConf.port, PostgresqlConf.db, 1, 1000 * 10, 1000 * 10)
         ConfigRegistered.getInstance.registered(postgresConf)
-        const dbIns = SF.getInstance.get(Store.Postgres)
-        // await dbIns.open()
-        const result = await JsonApiMain({event, db: dbIns})
-        await dbIns.close()
-        return result
+        return Main(event)
+        // ConfigRegistered.getInstance.registered(postgresConf)
+        // const dbIns = SF.getInstance.get(Store.Postgres)
+        // // await dbIns.open()
+        // const result = await JsonApiMain({event, db: dbIns})
+        // await dbIns.close()
+        // return result
     }
 }

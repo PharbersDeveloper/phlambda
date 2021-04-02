64ddeb5f445294108df6fd95c8791e1c612d5425
wodelu <1058744843@qq.com>
2021-04-02 23:11:38 +0800

    s3上传文件，返回地址链接

--- phtm/src/delegate/appLambdaDelegate.ts
+++ phtm/src/delegate/appLambdaDelegate.ts
@@ -2,17 +2,22 @@
 import { ConfigRegistered, Logger, Main, PostgresConfig, RedisConfig, SF, Store } from "phnodelayer"
 import { PostgresqlConf } from "../common/config"
 import { callRHandler } from "../handler/callRHandler"
+import { exportsHandler } from "../handler/exportsHandler"
 
 export default class AppLambdaDelegate {
     async exec(event: Map<string, any>) {
-        // @ts-ignore
-        if (event.pathParameters.type === "callR") {
-            return await callRHandler(event)
-        }
         const postgresConf = new PostgresConfig(PostgresqlConf.entry, PostgresqlConf.user,
             PostgresqlConf.password, PostgresqlConf.url,
             PostgresqlConf.port, PostgresqlConf.db, 1, 1000 * 10, 1000 * 10)
         ConfigRegistered.getInstance.registered(postgresConf)
+        // @ts-ignore
+        if (event.pathParameters.type === "callR") {
+            return await callRHandler(event)
+        }
+        // @ts-ignore
+        if (event.pathParameters.type === "export") {
+            return await exportsHandler(event)
+        }
         return Main(event)
         // ConfigRegistered.getInstance.registered(postgresConf)
         // const dbIns = SF.getInstance.get(Store.Postgres)

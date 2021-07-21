dd5e9d517f37293298a9d45e3ba181196cde7ec6
Alex <891366402@qq.com>
2021-07-21 18:45:14 +0800

    删除验证code

--- phcatlog/src/delegate/appLambdaDelegate.ts
+++ phcatlog/src/delegate/appLambdaDelegate.ts
@@ -1,7 +1,5 @@
 import { ServerResponse } from "http"
-import { identify } from "phauthlayer"
-import { AWSRequest, ConfigRegistered, Logger, RedisConfig, SF, Store } from "phnodelayer"
-import {RedisConf} from "../common/config"
+import { AWSRequest } from "phnodelayer"
 import GetGlueData from "../common/getGlueData"
 import ModelSerialize from "../common/modelSerialize"
 import Register from "../common/register"
@@ -10,13 +8,6 @@ import {AWSSts} from "../utils/AWSSts"
 
 export default class AppLambdaDelegate {
     async exec(event: any) {
-        ConfigRegistered.getInstance.registered(new RedisConfig(
-            RedisConf.entry, RedisConf.user, RedisConf.password,
-            RedisConf.url, RedisConf.port, RedisConf.db
-        ))
-        const rds = SF.getInstance.get(Store.Redis)
-        await rds.open()
-        const tokenResult = await rds.find("access", null, {match: {token: event.headers.Authorization}})
         try {
             const projectName = "catlog"
             const awsRequest = new AWSRequest(event, projectName)
@@ -34,37 +25,27 @@ export default class AppLambdaDelegate {
             register.registerFunction("tables", getGlueData.getTables)
             register.registerFunction("partitions", getGlueData.getPartitions)
 
-            let scope = ""
-            if (tokenResult.payload.records.length > 0) {
-                scope  = tokenResult.payload.records[0].scope
+            if (!event.body) {
+                event.body = ""
             }
-            const flag = identify(event, scope)
-            if (flag.status === 200) {
-                if (!event.body) {
-                    event.body = ""
-                }
-                const endpoint = event.pathParameters.type
-                const queryStringParameters = event.queryStringParameters
+            const endpoint = event.pathParameters.type
+            const queryStringParameters = event.queryStringParameters
 
-                if (endpoint === "databases") {
-                    const data = await register.getFunc(endpoint)(glueIns)
-                    awsResponse["output"] = ["", JSON.stringify(new ModelSerialize().serialize(endpoint, data))]
-                } else if (endpoint === "tables") {
-                    const data = await register.getFunc(endpoint)(glueIns, queryStringParameters["filter[database]"])
-                    awsResponse["output"] = ["", JSON.stringify(new ModelSerialize().serialize(endpoint, data))]
-                } else {
-                    const data = await register.getFunc(endpoint)(glueIns,
-                        queryStringParameters["filter[database]"],
-                        queryStringParameters["filter[table]"])
-                    awsResponse["output"] = ["", JSON.stringify(new ModelSerialize().serialize(endpoint, data))]
-                }
-                return awsResponse
+            if (endpoint === "databases") {
+                const data = await register.getFunc(endpoint)(glueIns)
+                awsResponse["output"] = ["", JSON.stringify(new ModelSerialize().serialize(endpoint, data))]
+            } else if (endpoint === "tables") {
+                const data = await register.getFunc(endpoint)(glueIns, queryStringParameters["filter[database]"])
+                awsResponse["output"] = ["", JSON.stringify(new ModelSerialize().serialize(endpoint, data))]
+            } else {
+                const data = await register.getFunc(endpoint)(glueIns,
+                    queryStringParameters["filter[database]"],
+                    queryStringParameters["filter[table]"])
+                awsResponse["output"] = ["", JSON.stringify(new ModelSerialize().serialize(endpoint, data))]
             }
-            return flag
+            return awsResponse
         } catch (error) {
             throw error
-        } finally {
-            await rds.close()
         }
     }
 }

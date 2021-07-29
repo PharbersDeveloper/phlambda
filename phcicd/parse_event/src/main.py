196a634c89bdd74628616117c146ba35dc87b2b4
hbzhao <15847059648@163.com>
2021-07-29 18:07:17 +0800

    实现push代码进行codebuild

--- phcicd/parse_event/src/main.py
+++ phcicd/parse_event/src/main.py
@@ -1,18 +1,22 @@
 import json
 import os
-from parse_event import Parse
-from git_python import GitRepository
+import subprocess
+from src.parse_event import Parse
+from src.git_python import GitRepository
+from src.package_code import zip_code
 
 def lambda_handler(event, context):
     parse = Parse()
-    print(event)
+
     git_event = parse.print_branch_information(event=event)
-    if git_event['event_type'] == "UPDATE":
+    if git_event['event_type'] == "UPDATE" and :
         git_url = 'https://hbzhao:123456@bitbucket.pharbers.com/scm/lgc/phlambda.git'
-        local_path_prefix = '/home/hbzhao/PycharmProjects/pythonProject/phlambda/phcicd/parse_event/src'
+        local_path_prefix = '/tmp'
         local_path = os.path.join(local_path_prefix, 'phlambda')
         repo = GitRepository(local_path, git_url, branch='feature/PBDP-1767-phlambda-cicd')
         print(repo.branches())
+
+    zip_code()
 
     return {
         "statusCode": 200,

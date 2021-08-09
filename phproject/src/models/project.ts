1b9cf48f18daefd1969cb9c24318cf687517a66b
Alex <891366402@qq.com>
2021-08-09 13:27:45 +0800

    修改model 新增execution

--- phproject/src/models/project.ts
+++ phproject/src/models/project.ts
@@ -2,11 +2,21 @@
 class Project {
     model: any = {
         project: {
+            arn: String,
             name: String,
             type: String,
             created: Number,
             provider: String,
+            define: String,
             tags: Object,
+        },
+        execution: {
+            arn: String,
+            name: String,
+            status: String,
+            startTime: Number,
+            endTime: Number,
+            tags: Object
         }
     }
 

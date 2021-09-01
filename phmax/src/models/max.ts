e4676a692bdc22360cb0a3a2c312ffb034340e0c
Alex <891366402@qq.com>
2021-09-01 17:33:42 +0800

    max log减少前端交互主要对joblog entiry进行操作

--- phmax/src/models/max.ts
+++ phmax/src/models/max.ts
@@ -1,3 +1,5 @@
+import { IStore, Register, StoreEnum } from "phnodelayer"
+
 class Max {
     model: any = {
         project: {
@@ -22,23 +24,72 @@ class Max {
 
     operations = {
         hooks: {
-            project: [this.hookProjectInput]
+            jobLog: [this.hookProjectInput]
         }
     }
 
-    protected async hookProjectInput(context, record, update) {
+    async hookProjectInput(context, record, update) {
         const { request: { method } } = context
+        const store = Register.getInstance.getData(StoreEnum.POSTGRES) as IStore
+        function clone(obj) {
+            const cloneObj = {}
+            for (const item of Object.keys(obj)) {
+                cloneObj[item] = record[item]
+            }
+            delete cloneObj["id"]
+            delete cloneObj["provider"]
+            delete cloneObj["time"]
+            return cloneObj
+        }
+        async function JobLogCreate() {
+            let createProject = await store.
+                find("project", null, { match: { provider: record.provider, time: record.time} })
+            const cloneRecord = clone(record)
+            if (createProject.payload.records.length === 0) {
+                createProject = await store.create("project", {
+                    provider: record.provider,
+                    time: record.time,
+                    actions: "[]"
+                })
+            }
+            const actions = JSON.parse(createProject.payload.records[0].actions)
+                .filter((item) => item.jobCat !== record.jobCat)
+            actions.push(cloneRecord)
+            const updateProject = {
+                id: createProject.payload.records[0].id,
+                replace: {
+                    actions: JSON.stringify(actions)
+                }
+            }
+            await store.update("project", updateProject)
+        }
+        async function JobLogUpdate() {
+            let resultProject = await store.
+                find("project", null, { match: { provider: record.provider, time: record.time} })
+            const cloneRecord = clone(record)
+            for (const item of Object.keys(update.replace)) {
+                cloneRecord[item] = update.replace[item]
+            }
+            const actions = JSON.parse(resultProject.payload.records[0].actions)
+                .filter((item) => item.jobCat !== record.jobCat)
+            actions.push(cloneRecord)
+            const updateProject = {
+                id: resultProject.payload.records[0].id,
+                replace: {
+                    actions: JSON.stringify(actions)
+                }
+            }
+            await store.update("project", updateProject)
+        }
         switch (method) {
+            case "create":
+                await JobLogCreate()
+                return record
             case "update":
-                const value = JSON.parse(update.replace.actions)
-                const actions = JSON.parse(record.actions)
-                    .filter((item) => item.jobCat !== value.jobCat)
-                actions.push(value)
-                update.replace.actions = JSON.stringify(actions)
+                await JobLogUpdate()
                 return update
         }
     }
-
 }
 
 export default Max

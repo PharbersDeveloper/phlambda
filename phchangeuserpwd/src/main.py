56a7805f84d0c4fc8c6ee9fd30cd8b640a375452
songmengen <2091038466>
2021-11-05 16:55:09 +0800

    密码正式更改
    开始密码更改的sql语句

--- phchangeuserpwd/src/main.py
+++ phchangeuserpwd/src/main.py
@@ -2,13 +2,17 @@ import os
 import psycopg2
 
 os_env = os.environ
+# os_env['DBNAME'] = 'phplatform'
+# os_env['USER'] = 'pharbers'
+# os_env['PASSWORD'] = 'Abcde196125'
+# os_env['HOST'] = 'ph-db-lambda.cngk1jeurmnv.rds.cn-northwest-1.amazonaws.com.cn'
+# os_env['PORT'] = '5432'
 
 
 def sqlCall(sql):
     conn = psycopg2.connect(dbname=os_env['DBNAME'], user=os_env['USER'], password=os_env['PASSWORD'],
                             host=os_env['HOST'], port=os_env['PORT'])
     cur = conn.cursor()
-    sql = sql
     cur.execute(sql)
     rows = cur.fetchall()
     conn.commit()
@@ -16,18 +20,37 @@ def sqlCall(sql):
     return rows
 
 
-def Re_Passwords(id, password, new_password):
-    if sqlCall("select id from account where id='{}'".format(id)) == []:
-        return '账户不正确'
-    elif sqlCall("select id from account where id='{}' and password='{}'".format(id, password)) == []:
-        return "密码不正确"
+def reword_sql(sql):
+    conn = psycopg2.connect(dbname=os_env['DBNAME'], user=os_env['USER'], password=os_env['PASSWORD'],
+                            host=os_env['HOST'], port=os_env['PORT'])
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
 
+def Re_Passwords(id, password, new_password):
+    if len(sqlCall("select id from account where id='{}'".format(id))) == 0:
+        return '账户不正确'
+    elif len(sqlCall("select id from account where id='{}' and password='{}'".format(id, password))) == 0:
+        return "密码不正确"
     else:
-        # sql = "UPDATE account SET password = '{}' WHERE id = '{}'".format(new_password, id)
-        # sql_call(sql)
+        sql = "UPDATE account SET password = '{}' WHERE id = '{}'".format(new_password, id)
+        reword_sql(sql)
         return "密码修改成功"
 
 
 def Run(event, context):
-    number = Re_Passwords(event['id'], event['password'], event['new_password'])
-    return number
+    result_str = Re_Passwords(event['id'], event['password'], event['new_password'])
+    return result_str
+
+
+# if __name__ == "__main__":
+#     import json
+#
+#     with open("../events/event.json", 'r') as f:
+#         data = json.load(f)
+#     data = json.loads(data['body'])
+#     # data['id'] = 'fdasfds'
+#     data['password'] = 'fsdafdsfsad'
+#     data['new_password'] = '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582'
+#     print(Run(data, ''))
--- phchangeuserpwd/test/test_phchupdatesql.py
+++ phchangeuserpwd/test/test_phchupdatesql.py
@@ -7,21 +7,21 @@ from phchangeuserpwd.src.main import Run
 
 class TestLmd:
     def test_lmd(self):
-        data = {'id': '5UBSLZvV0w9zh7-lZap',
+        data = {'id': 'fdasfds',
                 'password': '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582',
                 'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
         result = Run(data, ' ')
         assert "账户不正确" == result
 
     def test_lmd1(self):
-        data = {'id': '5UBSLZvV0w9zh7-lZQap',
+        data = {'id': 'qtaGDePl1OrSFEgm',
                 'password': '1cd7fc9d631b3541ae5f668e02e7c9472d9f0f56f83ccf2bc582',
                 'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
         result = Run(data, ' ')
         assert "密码不正确" == result
 
     def test_lmd2(self):
-        data = {'id': '5UBSLZvV0w9zh7-lZQap',
+        data = {'id': 'qtaGDePl1OrSFEgm',
                 'password': '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582',
                 'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
         result = Run(data, ' ')

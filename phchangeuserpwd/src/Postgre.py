4b0ac7807920d48d481f2e7702ce33ded0292aed
songmengen <2091038466>
2021-11-05 11:44:11 +0800

    环境变量修改
    环境变量名称不够标准，以及template.yaml中环境变量的添加

--- phchangeuserpwd/src/Postgre.py
+++ phchangeuserpwd/src/Postgre.py
@@ -1,16 +1,11 @@
 import os
 import psycopg2
 os_env = os.environ
-os_env['dbname'] = 'phplatform'
-os_env['user'] = 'pharbers'
-os_env['password'] = 'Abcde196125'
-os_env['host'] = 'ph-db-lambda.cngk1jeurmnv.rds.cn-northwest-1.amazonaws.com.cn'
-os_env['port'] = '5432'
 
 
 def SqlCall(sql):
-    conn = psycopg2.connect(dbname=os_env['dbname'], user=os_env['user'], password=os_env['password'],
-                            host=os_env['host'], port=os_env['port'])
+    conn = psycopg2.connect(dbname=os_env['DBNAME'], user=os_env['USER'], password=os_env['PASSWORD'],
+                            host=os_env['HOST'], port=os_env['PORT'])
     cur = conn.cursor()
     sql = sql
     cur.execute(sql)

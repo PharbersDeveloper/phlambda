62abab38d9f08495d3a39d2c46a224b67d0a7857
songmengen <2091038466>
2021-11-05 13:47:06 +0800

    测试更改
    定义密码函数更改，测试函数更改。

--- phchangeuserpwd/test/pytests.py
+++ phchangeuserpwd/test/pytests.py
@@ -1,42 +1,30 @@
 import pytest
-from phchangeuserpwd.src.app import Run as run
+from phchangeuserpwd.src.app import Run
 
 
 class TestLmd:
     def test_lmd(self):
-        data = {'email': 'pqian@pharbers.com', 'password': '6666666666', 'new_password': '999999'}
-        result = run(data, ' ')
-        print(result)
-        assert "password false" == result
+        data = {'id': '5UBSLZvV0w9zh7-lZap',
+                'password': '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582',
+                'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
+        result = Run(data, ' ')
+        assert "账户不正确" == result
 
     def test_lmd1(self):
-        data = {'email': 'pqian@pharbersom', 'password': 'Abcde196125', 'new_password': '999999'}
-        result = run(data, ' ')
-        print(result)
-        assert "account false" == result
+        data = {'id': '5UBSLZvV0w9zh7-lZQap',
+                'password': '1cd7fc9d631b3541ae5f668e02e7c9472d9f0f56f83ccf2bc582',
+                'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
+        result = Run(data, ' ')
+        assert "密码不正确" == result
 
     def test_lmd2(self):
-        data = {'email': 'pqian@pharbers.com', 'password': 'Abcde196125', 'new_password': '999999'}
-        result = run(data, ' ')
-        assert "password len false" == result
-
-    def test_lmd3(self):
-        data = {'email': 'pqian@pharbers.com', 'password': 'Abcde196125', 'new_password': '99999ffds9'}
-        result = run(data, ' ')
-        assert "password should format false" == result
-
-    def test_lmd4(self):
-        data = {'email': 'pqian@pharbers.com', 'password': 'Abcde196125', 'new_password': 'd经sf!9ffds9'}
-        result = run(data, ' ')
-        assert "password only format false" == result
-
-    def test_lmd5(self):
-        data = {'email': 'pqian@pharbers.com', 'password': 'Abcde196125', 'new_password': 'dsf!9ffds9'}
-        result = run(data, ' ')
-        assert "密码更改成功" == result
+        data = {'id': '5UBSLZvV0w9zh7-lZQap',
+                'password': '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582',
+                'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
+        result = Run(data, ' ')
+        assert "密码修改成功" == result
 
 
 if __name__ == '__main__':
     pytest.main()
-
 

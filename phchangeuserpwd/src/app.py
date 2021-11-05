ebdee4cdf63288ed0fd940f55a9e897e490e628d
songmengen <2091038466>
2021-11-05 13:43:19 +0800

    密码减少长度和格式验证
    因为前端传入密码为加密形式，所以修改以前加密的代码，减去密码格式验证服务

--- phchangeuserpwd/src/app.py
+++ phchangeuserpwd/src/app.py
@@ -2,12 +2,15 @@ from phchangeuserpwd.src.re_password import Re_Passwords
 
 
 def Run(event, context):
-    number = Re_Passwords(event['email'], event['password'], event['new_password']).items()
-    for i, j in number:
-        if j:
-            return i.replace('_', ' ')
-    else:
-        return '密码更改成功'
+    print(event)
+    number = Re_Passwords(event['id'], event['password'], event['new_password'])
+    return number
 
 
-
+if __name__ == "__main__":
+    import json
+    with open("../events/event.json", 'r') as f:
+        data = json.load(f)
+    data = json.loads(data['body'])
+    data['password'] = '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582'
+    print(Run(data, ''))

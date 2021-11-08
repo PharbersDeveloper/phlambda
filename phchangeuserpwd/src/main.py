d9baf18e870d1d8ba47939a2a1f6c8a58818f044
songmengen <2091038466>
2021-11-08 14:37:04 +0800

    修改测试文件
    线上测试

--- phchangeuserpwd/src/main.py
+++ phchangeuserpwd/src/main.py
@@ -2,8 +2,14 @@ import os
 import psycopg2
 import json
 os_env = os.environ
+os_env['DBNAME'] ="phplatform"
+os_env['USER'] ="pharbers"
+os_env['PASSWORD'] ="Abcde196125"
+os_env['HOST'] ="ph-db-lambda.cngk1jeurmnv.rds.cn-northwest-1.amazonaws.com.cn"
+os_env['PORT'] ="5432"
 conn = psycopg2.connect(dbname=os_env['DBNAME'], user=os_env['USER'], password=os_env['PASSWORD'],
                         host=os_env['HOST'], port=os_env['PORT'])
+
 
 
 def lambdaHandler(event, context):
@@ -21,8 +27,8 @@ def lambdaHandler(event, context):
         sql = "UPDATE account SET password='{}' WHERE id = '{}' AND password = '{}'".format(event['new_password'], event['id'], event['password'])
         cur.execute(sql)
         conn.commit()
+        conn.close()
         return {
             "statusCode": 200,
             "body": json.dumps("repassword success")
         }
-
--- phchangeuserpwd/test/test_phchangeuserpwd.py
+++ phchangeuserpwd/test/test_phchangeuserpwd.py
@@ -1,33 +1,18 @@
 import pytest
-import sys
-sys.path.insert(0, '../..')
-from phchangeuserpwd.src.main import Run
-
+from phchangeuserpwd.src.main import lambdaHandler
 
 
 class TestLmd:
     def test_lmd(self):
-        data = {'id': 'fdasfds',
-                'password': '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582',
-                'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
-        result = Run(data, ' ')
-        assert "账户不正确" == result
-
+        data = {
+            "body": "{\"id\": \"qtaGDePl1OrSFEgm\",\"password\": \"4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6\",\"new_password\": \"1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582\"}", }
+        result = lambdaHandler(data, ' ')
+        assert '"repassword False"' == result['body']
     def test_lmd1(self):
-        data = {'id': 'qtaGDePl1OrSFEgm',
-                'password': '1cd7fc9d631b3541ae5f668e02e7c9472d9f0f56f83ccf2bc582',
-                'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
-        result = Run(data, ' ')
-        assert "密码不正确" == result
-
-    def test_lmd2(self):
-        data = {'id': 'qtaGDePl1OrSFEgm',
-                'password': '1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582',
-                'new_password': '4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6'}
-        result = Run(data, ' ')
-        assert "密码修改成功" == result
-
+        data = {
+            "body": "{\"id\": \"qtaGDePl1OrSFEgm\",\"password\": \"1cd7fc9d631b3541354d5119236bae5f668e02e7c9472d9f0f56f83ccf2bc582\",\"new_password\": \"4c4cc360324690e719a628c83287201cb0a2e0e512b53c33d7135c7ba9dda9d6\"}", }
+        result = lambdaHandler(data, ' ')
+        assert '"repassword success"' == result['body']
 
 if __name__ == '__main__':
     pytest.main()
-

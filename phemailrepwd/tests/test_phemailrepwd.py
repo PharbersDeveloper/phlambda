d738edbdb8d174e13340dedff4a26515ffd45bf7
songmengen <2091038466>
2021-11-09 13:58:25 +0800

    邮件发送网站修改密码

--- phemailrepwd/tests/test_phemailrepwd.py
+++ phemailrepwd/tests/test_phemailrepwd.py
@@ -1,17 +1,23 @@
+import json
 import pytest
 from phemailrepwd.src.main import lambdaHandler
 
 
 class TestLmd:
     def test_lmd(self):
-        data = {"type": "test", "email": "2091038466@qq.com"}
-        result = lambdaHandler(data, ' ')
-        assert '"test_success"' == result['body']
+        data = {"type": "test", "address": "2091038466@qq.com", "subject": "密码修改", "attachment": None}
+        assert "修改密码的网址发送成功" == json.loads(lambdaHandler(data, ' ')['body'])
 
     def test_lmd1(self):
-        data = {"type": "forget_password", "email": "2091038466@qq.com"}
-        result = lambdaHandler(data, ' ')
-        assert '"repassword success"' == result['body']
+        attachment = [
+            {"file_name": "test1.yaml",
+             "file_context": ['PH_NOTICE_EMAIL:\n', '  metadata:\n', '    name: PH_NOTICE_EMAIL\n']},
+            {"file_name": "test2.txt", "file_context": ["xxxxxxxxxxxxxxxxxx"]},
+            # {"file_name" : "test3.png", "file_context" : ["xxxxxxxxxxxxxxxxxx"]}
+        ]
+        data = {"type": "forget_password", "address": "2091038466@qq.com", "subject": "密码修改",
+                "attachment": attachment}
+        assert "文件发送成功" == json.loads(lambdaHandler(data, ' ')['body'])
 
 
 if __name__ == '__main__':

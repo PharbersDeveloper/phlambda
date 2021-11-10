66d6ba597d5d61bb08d4af67dec5d2b60827cbd5
songmengen <2091038466>
2021-11-10 09:41:01 +0800

    -o

--- phemailrepwd/tests/test_phemailrepwd.py
+++ phemailrepwd/tests/test_phemailrepwd.py
@@ -5,19 +5,14 @@ from phemailrepwd.src.main import lambdaHandler
 
 class TestLmd:
     def test_lmd(self):
-        data = {"type": "test", "address": "2091038466@qq.com", "subject": "密码修改", "attachment": None}
-        assert "修改密码的网址发送成功" == json.loads(lambdaHandler(data, ' ')['body'])
+        data = {
+            "body": "{\"content_type\": \"test\", \"target_address\": [\"2091038466@qq.com\"],\n  \"subject\": \"密码修改\",\n  \"attachments\": [{\"file_name\": \"test1.yaml\",\n    \"file_context\": [\"PH_NOTICE_EMAIL:\", \"metadata:\", \"name: PH_NOTICE_EMAIL\"]},\n    {\"file_name\": \"test2.txt\", \"file_context\": [\"xxxxxxxxxxxxxxxxxx\"]}]}"}
+        assert "file_sucess" == json.loads(lambdaHandler(data, ' ')['body'])["message"]
 
     def test_lmd1(self):
-        attachment = [
-            {"file_name": "test1.yaml",
-             "file_context": ['PH_NOTICE_EMAIL:\n', '  metadata:\n', '    name: PH_NOTICE_EMAIL\n']},
-            {"file_name": "test2.txt", "file_context": ["xxxxxxxxxxxxxxxxxx"]},
-            # {"file_name" : "test3.png", "file_context" : ["xxxxxxxxxxxxxxxxxx"]}
-        ]
-        data = {"type": "forget_password", "address": "2091038466@qq.com", "subject": "密码修改",
-                "attachment": attachment}
-        assert "文件发送成功" == json.loads(lambdaHandler(data, ' ')['body'])
+        data = {
+            "body": "{\"content_type\": \"forget_password\", \"target_address\": [\"2091038466@qq.com\"],\n  \"subject\": \"密码修改\",\n  \"attachments\": [{\"file_name\": \"test1.yaml\",\n    \"file_context\": [\"PH_NOTICE_EMAIL:\", \"metadata:\", \"name: PH_NOTICE_EMAIL\"]},\n    {\"file_name\": \"test2.txt\", \"file_context\": [\"xxxxxxxxxxxxxxxxxx\"]}]}"}
+        assert "password_change_sucess" == json.loads(lambdaHandler(data, ' ')['body'])["message"]
 
 
 if __name__ == '__main__':

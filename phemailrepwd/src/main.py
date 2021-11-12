9a039e4c33f31bf55e7b07a883c8931461037dd4
songmengen <2091038466>
2021-11-12 15:15:40 +0800

    调试数据库

--- phemailrepwd/src/main.py
+++ phemailrepwd/src/main.py
@@ -13,15 +13,23 @@ from email import encoders
 # 1. first thing is to connect db, we use redis as tmp store
 r = redis.StrictRedis(host='pharbers-cache.xtjxgq.0001.cnw1.cache.amazonaws.com.cn', port=6379, db=0)
 # r = redis.StrictRedis(host='localhost', port=6379, db=1)
-
+os.environ['USER'] = 'project@data-pharbers.com'
+os.environ['BUCKET'] = "ph-platform"
+os.environ['KEY_PWD'] = "2020-11-11/template/email/email-forgetpwd.html"
+os.environ['KEY_FILE'] = "2020-11-11/template/email/email.html"
+os.environ['PSWD'] = 'qiye@126'
+os.environ['HOST'] = 'smtp.ym.163.com'
+os.environ['PORT'] = '465'
+os.environ['SENDER'] = 'project@data-pharbers.com'
+os.environ['SENDER_NAME'] = "法伯科技"
 # 2. other version
-g_sender_name = os.environ("SENDER_NAME")
-g_host = os.environ("HOST")
-g_port = os.environ("PORT")
-g_username = os.environ("USER")
-g_pwd = os.environ("PSWD")
-g_sender = os.environ("SENDER")
-g_bucket = os.enviroon("BUCKET")  # "ph-platform"
+g_sender_name = os.environ["SENDER_NAME"]
+g_host = os.environ["HOST"]
+g_port = os.environ["PORT"]
+g_username = os.environ["USER"]
+g_pwd = os.environ["PSWD"]
+g_sender = os.environ["SENDER"]
+g_bucket = os.environ["BUCKET"]  # "ph-platform"
 # g_key = "ph-platform"
 
 # 3. global email ssl
@@ -31,12 +39,12 @@ server.login(g_username, g_pwd)
 
 def loadContent(ttype, address):
     type_key = {
-        "forget_password": "bu",
-        "test": "bu"
+        "forget_password": os.environ['KEY_PWD'],
+        "test": os.environ['KEY_FILE']
     }
     func = {
         "forget_password": forgetPwdFunc,
-        "test": testFunc
+        "test": tesFunc
     }
 
     client = boto3.client('s3')
@@ -72,60 +80,65 @@ def sendEmail(address, content_type, html_content, attachments=None, content_sty
             att["Content-Disposition"] = "'attachment; filename=" + attach_file['file_name'] + " '"
             msg.attach(att)
     server.sendmail(g_sender, address, msg.as_string())
-
+    server.quit
 
 def forgetPwdFunc(ctx, address):
-    tmp = ''.join([random.randint(1, 9) for i in range(6)])
+    tmp = ''.join([str(random.randint(1, 9)) for i in range(6)])
+    print(666)
+    print(address, tmp)
     r.set(address, tmp)
-    return ctx.replace("%%%%URL%%%%", tmp)
+    print(777)
+    return ctx.replace("$$$验证码$$$", tmp)
 
 
-def testFunc(ctx):
-    return ctx
+def tesFunc(ctx, address):
+    tmp = ''.join([str(random.randint(1, 9)) for i in range(6)])
+    r.set(address, tmp)
+    return ctx.replace("$$$验证码$$$", tmp)
 
 
 def lambdaHandler(event, context):  # 主函数入口
     result_code = 200
     result_message = {}
-    try:
-        event = event['body']
-        event = json.loads(event)
-        for address in event['target_address']:
-            sendEmail(address=address,
-                      content_type=event['content_type'],
-                      html_content=loadContent(event['content_type'], address),
-                      attachments=event['attachments'],
-                      content_style="html")
-        result_message = {
-            "status": "ok",
-            "data": {
-                "message": "seccessfully send emails"
-            }
+    # try:
+    event = event['body']
+    event = json.loads(event)
+    for address in event['target_address']:
+        sendEmail(address=address,
+                  content_type=event['content_type'],
+                  html_content=loadContent(event['content_type'], address),
+                  attachments=event['attachments'],
+                  content_style="html")
+    result_message = {
+        "status": "ok",
+        "data": {
+            "message": "seccessfully send emails"
         }
-    except smtplib.SMTPServerDisconnected as e:
-        result_message = {
-            "status": "error",
-            "error": {
-                "message": "SMTP server disconnected"
-            }
-        }
-    except smtplib.SMTPResponseException as e:
-        result_message = {
-            "status": "error",
-            "error": {
-                "message": "SMTP server send email error"
-            }
-        }
-    except Exception as e:
-        result_message = {
-            "status": "error",
-            "error": {
-                "message": "Unknown Error"
-            }
-        }
-
-    if result_message["status"] == "error":
-        result_code = 503
+    }
+    # except smtplib.SMTPServerDisconnected as e:
+    #     result_message = {
+    #         "status": "error",
+    #         "error": {
+    #             "message": "SMTP server disconnected"
+    #         }
+    #     }
+    # except smtplib.SMTPResponseException as e:
+    #     result_message = {
+    #         "status": "error",
+    #         "error": {
+    #             "message": "SMTP server send email error"
+    #         }
+    #     }
+    # except Exception as e:
+    #     result_message = {
+    #         "status": "error",
+    #         "error": {
+    #             "message": "Unknown Error"
+    #         }
+    #     }
+    #
+    # if result_message["status"] == "error":
+    #     result_code = 503
 
     return {
         "statusCode": result_code,
@@ -136,3 +149,7 @@ def lambdaHandler(event, context):  # 主函数入口
         },
         "body": json.dumps(result_message)
     }
+if __name__ == "__main__":
+    data = {
+        "body": "{\"content_type\": \"forget_password\", \"target_address\": [\"2091038466@qq.com\"],\n  \"subject\": \"密码修改\",\n  \"attachments\": [{\"file_name\": \"test1.yaml\",\n    \"file_context\": [\"PH_NOTICE_EMAIL:\", \"metadata:\", \"name: PH_NOTICE_EMAIL\"]},\n    {\"file_name\": \"test2.txt\", \"file_context\": [\"xxxxxxxxxxxxxxxxxx\"]}]}"}
+    print(lambdaHandler(data, ' '))

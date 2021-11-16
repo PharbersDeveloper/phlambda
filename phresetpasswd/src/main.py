94713724a797f2baed1fcea13e0e0da5b3e681b0
songmengen <2091038466>
2021-11-16 17:01:36 +0800

    去掉token函数

--- phresetpasswd/src/main.py
+++ phresetpasswd/src/main.py
@@ -2,8 +2,8 @@ import random
 import boto3
 import redis
 import json
-import time
-import jwt
+# import time
+# import jwt
 
 r = redis.StrictRedis(host='pharbers-cache.xtjxgq.0001.cnw1.cache.amazonaws.com.cn', port=6379, db=1)
 
@@ -25,20 +25,20 @@ def errorMessage(e):
     return result_code, result_message
 
 
-def creat_token(email_address):
-    data = {
-        # 公共声明
-        "iat": int(time.time()),  # 生效时间
-        "exp": int(time.time()) + 300,  # 过期时间
-        'iss': 'https://www.accounts.pharbers.com:4200/resetPasswordPage/',  # 发布者url地址
-        # 私有声明
-        "data": {
-            "email": email_address
-        }
-    }
-    # jwt 加密 payload 加密数据 key加密签名的钥匙 algorithm 用什么方法加密
-    jwt_encode = jwt.encode(payload=data, key='123456', algorithm='HS256')
-    return jwt_encode
+# def creat_token(email_address):
+#     data = {
+#         # 公共声明
+#         "iat": int(time.time()),  # 生效时间
+#         "exp": int(time.time()) + 300,  # 过期时间
+#         'iss': 'https://www.accounts.pharbers.com:4200/resetPasswordPage/',  # 发布者url地址
+#         # 私有声明
+#         "data": {
+#             "email": email_address
+#         }
+#     }
+#     # jwt 加密 payload 加密数据 key加密签名的钥匙 algorithm 用什么方法加密
+#     jwt_encode = jwt.encode(payload=data, key='123456', algorithm='HS256')
+#     return jwt_encode
 
 
 def lambdaHandler(event, context):  # 主函数入口

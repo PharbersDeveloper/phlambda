5a7adf09ebc9985f0d23237dd59e2f598c6d1838
ylzhang <2824439675@qq.com>
2021-11-26 10:15:00 +0800

    change test

--- phchdatasource/test/test_phchdatasource.py
+++ phchdatasource/test/test_phchdatasource.py
@@ -5,8 +5,45 @@ from src.main import lambda_handler
 
 
 class TestLmd:
-    def test_lmd(self):
-        with open('../event/event.json', 'r', encoding='utf8') as fp:
+
+    def test_lmd_adapter_dict(self):
+        with open('../event/event_adapter_dict.json', 'r', encoding='utf8') as fp:
+            event = (json.loads(fp.read()))
+            report = lambda_handler(event, None)
+            print(report)
+
+    def test_lmd_adapter_error(self):
+        with open('../event/event_adapter_error.json', 'r', encoding='utf8') as fp:
+            event = (json.loads(fp.read()))
+            report = lambda_handler(event, None)
+            print(report)
+
+    def test_lmd_adapter_list(self):
+        with open('../event/event_adapter_list.json', 'r', encoding='utf8') as fp:
+            event = (json.loads(fp.read()))
+            report = lambda_handler(event, None)
+            print(report)
+
+    def test_lmd_default_default(self):
+        with open('../event/event_default_default.json', 'r', encoding='utf8') as fp:
+            event = (json.loads(fp.read()))
+            report = lambda_handler(event, None)
+            print(report)
+
+    def test_lmd_default_max(self):
+        with open('../event/event_default_max.json', 'r', encoding='utf8') as fp:
+            event = (json.loads(fp.read()))
+            report = lambda_handler(event, None)
+            print(report)
+
+    def test_lmd_default_None(self):
+        with open('../event/event_default_None.json', 'r', encoding='utf8') as fp:
+            event = (json.loads(fp.read()))
+            report = lambda_handler(event, None)
+            print(report)
+
+    def test_lmd_default_error(self):
+        with open('../event/event_default_error.json', 'r', encoding='utf8') as fp:
             event = (json.loads(fp.read()))
             report = lambda_handler(event, None)
             print(report)

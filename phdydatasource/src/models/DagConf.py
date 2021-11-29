fb0046f1af9354479a947f6067c13a30900d92da
Alex <891366402@qq.com>
2021-11-29 10:54:16 +0800

    DagConf新增jobPath

--- phdydatasource/src/models/DagConf.py
+++ phdydatasource/src/models/DagConf.py
@@ -5,8 +5,9 @@ from models.Model import Model
 class DagConf(Model):
     type = "dag-confs"
     attributes = {
-        "flowVersion": fields.Str(required=True),
-        "jobName": fields.Str(dump_default="unknown"),
+        "projectId": fields.Str(required=True),
+        "jobName": fields.Str(required=True),
+        "flowVersion": fields.Str(dump_default="unknown"),
         "dagName": fields.Str(dump_default="unknown"),
         "inputs": fields.Str(dump_default="unknown"),
         "jobDisplayName": fields.Str(dump_default="unknown"),
@@ -14,11 +15,11 @@ class DagConf(Model):
         "jobVersion": fields.Str(dump_default="unknown"),
         "outputs": fields.Str(dump_default="unknown"),
         "owner": fields.Str(dump_default="unknown"),
-        "projectId": fields.Str(dump_default="unknown"),
         "runtime": fields.Str(dump_default="unknown"),
         "targetJobId": fields.Str(dump_default="unknown"),
         "timeout": fields.Str(dump_default="unknown"),
         "labels": fields.Str(dump_default="unknown"),
-        "projectName": fields.Str(dump_default="unknown")
+        "projectName": fields.Str(dump_default="unknown"),
+        "jobPath": fields.Str(dump_default="unknown")
     }
 

bcac3766c69e9f9ea107e77a799170c64b62c4bb
Alex <891366402@qq.com>
2021-11-30 21:00:24 +0800

    fix query ClickHouse中Table数据的版本数量

--- phcheckversion/src/main.py
+++ phcheckversion/src/main.py
@@ -6,6 +6,7 @@ import urllib.parse
 def executeSql(sql, type):
     conn = http.client.HTTPConnection(host="192.168.16.117", port="8123")
     url = urllib.parse.quote("/ch/?query=" + sql, safe=":/?=&")
+    print(url)
     conn.request(type, url)
     res = conn.getresponse()
     return res.read().decode("utf-8")
@@ -29,7 +30,7 @@ def lambda_handler(event, context):
         tmp = {}
         for cell in cells:
             index = cells.index(cell)
-            tmp[columns[index]] = cell
+            tmp[columns[index]] = cell if cell in "UNKNOWN_TABLE" else "0"
 
         final_res.append(tmp)
 

a0b48b7f21c155bc0b37a935ba212dcf7b58ed88
ylzhang <2824439675@qq.com>
2021-11-30 18:21:31 +0800

    删除注释

--- phdadownload/src/main.py
+++ phdadownload/src/main.py
@@ -1,24 +1,11 @@
-# import json
-# from Common.ExportData.ChoiceMain import ChoiceMain
-#
-#
-# def lambda_handler(event, context):
-#
-#     return {
-#         "statusCode": 200,
-#         "headers": {"Access-Control-Allow-Origin": "*"},
-#         "body": ChoiceMain().choice(event)
-#     }
-# ssm get url
-
-
 import json
-import boto3
+from Common.ExportData.ChoiceMain import ChoiceMain
 
-ssm_client = boto3.client('ssm', region='cn-northwest-1')
-response = ssm_client.get_parameter(
-    Name='projects_driver_args',
-    WithDecryption=True|False
-)
-ssm_dict = json.loads(response.get('Parameter').get('Value'))
-print(ssm_dict)
+
+def lambda_handler(event, context):
+
+    return {
+        "statusCode": 200,
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": ChoiceMain().choice(event)
+    }

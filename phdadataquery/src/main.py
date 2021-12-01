44629e6a9a71fc09cdc87bade3f7a5d39c3c6fb1
ylzhang <2824439675@qq.com>
2021-12-01 17:26:06 +0800

    query

--- phdadataquery/src/main.py
+++ phdadataquery/src/main.py
@@ -1,6 +1,5 @@
 import json
 import boto3
-import datetime
 import time
 
 
@@ -14,22 +13,15 @@ def down_data_with_time(bucket, key, file_name, **kwargs) -> tuple:
     return binaryarray, int(time.mktime(last_time.timetuple()))+3600*8
 
 
-def up_data(bucket, key, file_name, data):
+# def up_data(bucket, key, file_name, data):
     # s3 = boto3.client('s3')
     # s3.put_object(Body=data.encode(), Bucket=bucket, Key=(key+file_name))
-    print(1)
+    # print(1)
 
 
-def run(timespan, **kwargs):
+def run(**kwargs):
     data, last_time = down_data_with_time(**kwargs)
-    print(last_time)
-    print(data)
-
-    if int(timespan) > last_time:
-        up_data(**kwargs)
-        return 'up data success'
-    else:
-        return 'timespan error'
+    return {"data": data, "timespan": last_time}
 
 
 def lambdaHandler(event, context):
@@ -41,7 +33,7 @@ def lambdaHandler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH,DELETE"
         },
-        "body": run(**eval(event["body"]))
+        "body": json.dumps(run(**eval(event["body"])), ensure_ascii=False)
     }
 
 

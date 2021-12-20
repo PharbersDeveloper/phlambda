b62db9d622f3f8f545aafd44002db6d5e56049cd
ylzhang <2824439675@qq.com>
2021-12-20 17:37:39 +0800

    读取 log 日志

--- phquerylogfile/src/main.py
+++ phquerylogfile/src/main.py
@@ -2,36 +2,60 @@ import json
 import boto3
 from io import BytesIO
 import gzip
+import base64
 
 
+s3 = boto3.client('s3')
 def if_exit(bucket, key):
+    # response = s3.get_object(Bucket='ph-platform',
+    #                          Key='2020-11-11/emr/yarnLogs/hadoop/logs-tfile/application_1639098106143_0120/ip-192-168-27-243.cn-northwest-1.compute.internal_8041')
+    # a = response.get('Body').read()
+    # print(a)
     _s3 = boto3.client('s3')
     try:
-        response = _s3.get_object(Bucket=bucket, Key=key+"stderr.gz")
-        print(response)
+        response = _s3.get_object(Bucket=bucket, Key=key)
     except:
         return False
     else:
-        return True
+        base64_data = base64.b64encode(response.get('Body').read())
+        base64_str = base64_data.decode('utf-8')
+        return base64_str
 
 
-def read_s3(bucket, key, **kwargs):
-    if not if_exit(bucket, key):
-        return
-
+def read_gz(bucket, key):
     s3 = boto3.resource('s3')
-    obj = s3.Object(bucket, key+"stderr.gz")
-    n = obj.get()['Body'].read()
-    gzipfile = BytesIO(n)
-    gzipfile = gzip.GzipFile(fileobj=gzipfile)
-    content = gzipfile.read()
-    # print(content.decode())
-    return content.decode()
+    obj = s3.Object(bucket, key)
+    gzipfile = BytesIO(obj.get()['Body'].read())
+    return gzip.GzipFile(fileobj=gzipfile).read()
+
+
+def query_logfile(bucket, file_key):
+    s3 = boto3.client("s3")
+    response = s3.list_objects_v2(
+        Bucket=f'{bucket}',
+        Prefix=f'2020-11-11/emr/yarnLogs/hadoop/logs-tfile/{file_key}/',
+        MaxKeys=100)
+    return [i.get('Key') for i in response.get('Contents', {})]
+
+
+def run(bucket, key, **kwargs):
+    if not if_exit(bucket, key+"stderr.gz"):
+        return
+    log_file = read_gz(bucket, key+"stderr.gz").decode()
+    file_nmae = log_file[log_file.rfind('application_'): log_file.rfind('application_')+30]
+    logkey_list = query_logfile(bucket, file_nmae)
+
+    # with open('test1.txt', 'wb') as f:
+    #     for i in logkey_list:
+    #         f.write(if_exit(bucket, i))
+
+    return [if_exit(bucket, i) for i in logkey_list]
 
 
 def lambda_handler(event, context):
     try:
-        data = read_s3(**eval(event["body"]))
+        data = run(**eval(event["body"]))
+        print(data)
         if not data:
             return {
                         "statusCode": 200,
@@ -40,8 +64,6 @@ def lambda_handler(event, context):
                         },
                         "body": json.dumps({"message": "error", "status": 0}, ensure_ascii=False)
                     }
-        rdata = data[data.rfind('application_'): data.rfind('application_')+30]
-        print(rdata)
 
     # application_1639098106143_0120
     except Exception as e:
@@ -58,5 +80,7 @@ def lambda_handler(event, context):
             "headers": {
                 "Access-Control-Allow-Origin": "*",
             },
-            "body": json.dumps({"message": rdata, "status": 1}, ensure_ascii=False)
+            "body": json.dumps({"message": data, "status": 1}, ensure_ascii=False)
         }
+    # data = run(**eval(event["body"]))
+    # print(data)

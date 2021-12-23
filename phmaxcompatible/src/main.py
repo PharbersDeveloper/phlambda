53bab6dedd2d3974697fca56aaf19d82dd0af341
ylzhang <2824439675@qq.com>
2021-12-23 15:01:06 +0800

    change input

--- phmaxcompatible/src/main.py
+++ phmaxcompatible/src/main.py
@@ -1,12 +1,10 @@
 
-import os
 import json
 import time
 import base64
 from util.AWS.STS import STS
 from constants.Common import Common
 from util.AWS.DynamoDB import DynamoDB
-from boto3.dynamodb.conditions import Attr, Key
 
 
 class AppLambdaDelegate:
@@ -18,22 +16,15 @@ class AppLambdaDelegate:
         )
         self.dynamodb = DynamoDB(sts=sts)
 
-    def run(self):
-        try:
-            self.dynamodb.putData({"table_name": "dataset", "item": self.dataset})
-            self.dynamodb.putData({"table_name": "notification", "item": self.notification})
-            return True
-        except:
-            return False
-
     @property
     def dataset(self):
         return {
-            "id": self.data.get("id"),
+            "id": self.data.get("dataset_id"),
             "projectId": self.data.get("projectId"),
             "date": int(time.time()),
-            "label": self.data.get("message"),
+            "label": "[]",
             "name": self.data.get("name"),
+            "schema": self.data.get("message"),
             "version": self.data.get("version")
         }
 
@@ -53,6 +44,31 @@ class AppLambdaDelegate:
             "showName": self.data.get("showName"),
         }
 
+    @property
+    def dag(self):
+        return {
+            "projectId": self.data.get("projectId"),
+            "sortVersion": self.data.get("version") + self.data.get("dataset_id"),
+            "ctype": "node",
+            "cat": "dataset",
+            "level": "-99999",
+            "runtime": "uploaded",
+            "cmessage": '',
+            "flowVersion": self.data.get("version"),
+            "name": self.data.get("name"),
+            "position": '{"x": "0", "y": "0", "z": "0", "w": "0", "h": "0"}',
+            "representId": self.data.get("dataset_id"),
+        }
+
+    def run(self):
+        try:
+            self.dynamodb.putData({"table_name": "dataset", "item": self.dataset})
+            self.dynamodb.putData({"table_name": "notification", "item": self.notification})
+            self.dynamodb.putData({"table_name": "dag", "item": self.dag})
+            return True
+        except:
+            return False
+
 
 def lambda_handler(event, context):
     records = event["Records"]
@@ -64,9 +80,11 @@ def lambda_handler(event, context):
             new_image = record["dynamodb"]["NewImage"]
             if record["eventName"].lower() == "insert" and \
                     new_image.get("jobCat", {"S": "None"})["S"] == "max1.0":
+
                 data = {
-                    "id": "e0d24d609b7fbf5b84918ec7720feaef.xlsx",
-                    "projectId": new_image.get("jobCat")["S"],
+                    "id": new_image.get("id")["S"],
+                    "dataset_id": json.loads(new_image.get("message")["S"]).get('id'),
+                    "projectId": new_image.get("projectId")["S"],
                     "category": "",
                     "code": 0,
                     "comments": "",
@@ -80,6 +98,6 @@ def lambda_handler(event, context):
                 }
                 app = AppLambdaDelegate(data)
                 app.run()
-
+        return True
     except:
-        pass
+        return False

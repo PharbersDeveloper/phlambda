97e369e9b3eb646774e1a5786df0e3ffd44f3454
AlfredYang1986 <yangyuanpig@gmail.com>
2022-02-09 11:41:11 +0800

    Update platform.ts

--- phplatform/src/models/platform.ts
+++ phplatform/src/models/platform.ts
@@ -309,6 +309,14 @@ export default class Platform {
             cat: String,
             level: Number,
             engine: String
+        },
+        // configuration web layout
+        layout: {
+            clientId: String,
+            clientName: String,
+            version: String,
+            name: String,
+            css: String
         }
     }
 

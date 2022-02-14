d3fb315d95946de44bcc028beeb29b71012b5ddf
Alex <891366402@qq.com>
2022-02-14 13:43:54 +0800

    fix throw Exception Error
    修复重复列名、检测Schema一致性 抛出异常的错误

--- phxlsxtockhouse/src/handler/Command/CheckSchemaReceiver.py
+++ phxlsxtockhouse/src/handler/Command/CheckSchemaReceiver.py
@@ -12,7 +12,7 @@ class CheckSchemaReceiver(Receiver):
         self.logger.debug(f"Alex Check Schema ====> \n {data}")
         cur_schema = list(map(lambda item: item["src"], data["cur_schema"]))
         ds_schema = list(map(lambda item: item["src"], data["ds_schema"]))
-        if len(set(ds_schema) - set(cur_schema)) > 0:  # 检测 Schema 在多次上传时是否与以前一直
+        if len(ds_schema) > 0 and (len(set(ds_schema)) - len(set(cur_schema))) != 0:  # 检测 Schema 在多次上传时是否与以前一直
             raise SchemaNotMatched("Schema Not Matched")
 
         if len(cur_schema) != len(set(cur_schema)):  # 检测 Schema 中是否有重复的列

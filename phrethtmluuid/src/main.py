8e3511ec725d6cba7e9745bcf5c1ada413c07cb6
张玉磊 <ylzhang@data-pharbers.com>
2022-03-17 07:25:29 +0000

    main.py edited online with Bitbucket

--- phrethtmluuid/src/main.py
+++ phrethtmluuid/src/main.py
@@ -11,8 +11,9 @@ def read_file():
         line = line.replace("\n", '')
         data_list = line.split(",")
         data["id"] = data_list[0]
-        data["title"] = data_list[1]
+        data["title"] = data_list[1].replace(' ', '')
         data["url"] = f'https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/html/{data_list[0]}.html'
+        data["date"] = data_list[2].replace(' ', '')
         last_data_list.append(data)
     print(last_data_list)
     return last_data_list

89132a16b5a319f0e343bae0ba3e4cbe9d63e479
张玉磊 <ylzhang@data-pharbers.com>
2022-03-17 06:40:41 +0000

    main.py edited online with Bitbucket

--- phrethtmluuid/src/main.py
+++ phrethtmluuid/src/main.py
@@ -4,15 +4,20 @@ from ExecHandler import html
 
 
 def read_file():
-    data = {}
+    last_data_list = []
     file = open('data.txt')
     for line in file.readlines():
+        data = {}
         line = line.replace("\n", '')
         data_list = line.split(",")
-        data[data_list[0]] = data_list[1]
-    return {"data": data}
+        data["id"] = data_list[0]
+        data["title"] = data_list[1]
+        data["url"] = f'https://s3.cn-northwest-1.amazonaws.com.cn/general.pharbers.com/html/{data_list[0]}.html'
+        last_data_list.append(data)
+    print(last_data_list)
+    return last_data_list
 
 
 def lambda_handler(event, context):
-    print(json.dumps(html(read_file())))
     return json.dumps(html(read_file()))
+

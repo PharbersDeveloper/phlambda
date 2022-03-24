04fd6695e6593632a5791cc3d29bab87261fd3c2
ylzhang <2824439675@qq.com>
2022-03-24 19:14:30 +0800

    dev 代码 更新

--- phdadataupdata/src/main.py
+++ phdadataupdata/src/main.py
@@ -1,7 +1,9 @@
 import json
+import json
 import boto3
 import datetime
 import time
+import urllib
 
 
 def down_data_with_time(bucket, key, file_name, **kwargs) -> tuple:
@@ -9,12 +11,11 @@ def down_data_with_time(bucket, key, file_name, **kwargs) -> tuple:
     response = client.get_object(Bucket=bucket, Key=(key+file_name))
     binaryarray = response.get('Body').read().decode()
     last_time = response.get('LastModified')
-    print(response)
-    print(last_time)
     return binaryarray, int(time.mktime(last_time.timetuple()))+3600*8
 
 
 def up_data(bucket, key, file_name, data):
+    data = urllib.parse.unquote(data)
     s3 = boto3.client('s3')
     s3.put_object(Body=data.encode(), Bucket=bucket, Key=(key+file_name))
 

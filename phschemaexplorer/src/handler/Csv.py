49c2d2267e38bdd466e6b2e608371a6459495d2d
AlfredYang1986 <yangyuanpig@gmail.com>
2022-03-30 16:55:21 +0800

    csv

--- phschemaexplorer/src/handler/Csv.py
+++ phschemaexplorer/src/handler/Csv.py
@@ -1,5 +1,41 @@
 from Strategy import Strategy
 
+from constants.Errors import Errors, FileNotFound, NotCsvFile
+from handler.Strategy import Strategy
+from openpyxl.utils.exceptions import InvalidFileException
+import os
+# import openpyxl
+import constants.Common as Common
+import logging
+import pandas as pd
 
 
+class Csv(Strategy):
 
+    def __init__(self):
+        pass
+
+    def do_exec(self, data):
+        logging.debug("Excel Xlsx === \n")
+        logging.debug(data)
+
+
+        result = {}
+        try:
+            skip_first = data["skip_first"]
+            skip_next = data["skip_next"]
+            project = data["project"]
+            temp_file = data["tempfile"]
+
+            // csv
+
+            result["sheets"] = ["csv"]
+            result["body"] = # TODO: ...
+        except FileNotFoundError as e:
+            raise FileNotFound(e)
+        except InvalidFileException as e:
+            raise NotCsvFile(e)
+        except Exception as e:
+            raise Errors(e)
+
+        return result

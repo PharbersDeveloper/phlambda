17fcf011e49d9473abb74dbf809a6de01cda07d4
mzhang <example.qq.com>
2022-03-30 14:09:00 +0800

    lambda function phcleanprocessfiles publish to v2

--- phcleanprocessfiles/src/main.py
+++ phcleanprocessfiles/src/main.py
@@ -5,6 +5,7 @@ from utils.cleanexpiredfile import CleanExpiredFile, SearchFilesPath
 
 # 运行指定dag
 def lambda_handler(event, context):
+
     base_path = os.getenv("BASE_PATH")
     expired_time = os.getenv("EXPIRED_TIME")
     waitting_clean_dirs = SearchFilesPath(base_path).handle_base_path()

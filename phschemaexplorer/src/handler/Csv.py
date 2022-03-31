b5dae9fb55bcfaa770490c9d8e46a13072b1edfe
ylzhang <2824439675@qq.com>
2022-03-31 12:00:56 +0800

    添加csv处理

--- phschemaexplorer/src/handler/Csv.py
+++ phschemaexplorer/src/handler/Csv.py
@@ -15,22 +15,30 @@ class Csv(Strategy):
     def __init__(self):
         pass
 
+    def __get_data(self, temp_file, skip_next, out_number=0, **kwargs):
+        skip_next = int(skip_next)
+        out_number = int(out_number)
+        out_num = out_number if out_number > 0 else 20
+        data = pd.read_csv(temp_file, encoding="utf-8")
+        body = data.values.tolist()
+        body = body[skip_next:]
+        body = body[:out_num]
+        return {'readNumber': 1,
+                'sheet': 'csv',
+                'schema': data.columns.values.tolist(),
+                'data': body
+                }
+
     def do_exec(self, data):
         logging.debug("Excel Xlsx === \n")
         logging.debug(data)
 
-
-        result = {}
         try:
-            skip_first = data["skip_first"]
-            skip_next = data["skip_next"]
             project = data["project"]
             temp_file = data["tempfile"]
+            path = f"{os.environ.get(Common.PATH_PREFIX).format(project)}{temp_file}"
+            csv_data = self.__get_data(temp_file=path, **data)
 
-            // csv
-
-            result["sheets"] = ["csv"]
-            result["body"] = # TODO: ...
         except FileNotFoundError as e:
             raise FileNotFound(e)
         except InvalidFileException as e:
@@ -38,4 +46,7 @@ class Csv(Strategy):
         except Exception as e:
             raise Errors(e)
 
-        return result
+        return {
+            'sheets': ["csv"],
+            'body': [csv_data]
+            }

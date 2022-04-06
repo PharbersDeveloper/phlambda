42c475534ba315a60383bbebf842e51820cbac38
mzhang <example.qq.com>
2022-04-06 17:17:52 +0800

    filter jobcat and take event message to sqs ,coding is ok ,sqs need create

--- phforwarddyevent/src/util/AWS/SQS.py
+++ phforwarddyevent/src/util/AWS/SQS.py
@@ -1,6 +1,22 @@
 import boto3
 import json
 from util.GenerateID import GenerateID
+import re
+
+
+_jobcat_queue_urls = {
+    "": "https://sqs.cn-northwest-1.amazonaws.com.cn/444603803904/ph-dsevent-pipeline.fifo",
+    "project_file_to_DS": "",
+    "remove_DS": "",
+    "clear_DS_data": "",
+    "transform_schema": "",
+    "project_files": "",
+    "resource_create": "",
+    "resource_delete": "",
+    "dag_refresh": "",
+    "dag_create": "",
+    "edit_sample":"",
+}
 
 class SQS():
 
@@ -8,15 +24,35 @@ class SQS():
 
         self.sqs_client = boto3.client('sqs')
 
-    def sqs_send_message(self, message):
-        print("message id ========================")
+    def handle_jobcat(self, event_dict):
+        def search_info(string_line, key):
+            patter_rule = f" .({key}).: ..S.: .(.*?).., "
+            search_result = re.findall(pattern=patter_rule, string=str(string_line))
+            return search_result
+        jobcat_name = search_info(event_dict, "jobCat")
+        #jobdesc = search_info(event_dict, "jobDesc")
+        return jobcat_name, _jobcat_queue_urls[jobcat_name][0][0]
 
+    def sqs_send_message(self, message):
+       #--基于jobcat 分发
+       try:
+            print("发消息到队列...成功")
+            jobcat_name, Queue_Url = self.handle_jobcat(message)
+            print("message id ========================")
+            response = self.sqs_client.send_message(
+                QueueUrl=Queue_Url,
+                MessageBody=json.dumps(message, ensure_ascii=False),
+                MessageGroupId=GenerateID().generate()
+            )
+       except Exception as e:
+           print("发消息到队列...失败")
+           print(str(e))
+       '''
         # response = self.sqs_client.send_message(
         #     QueueUrl="https://sqs.cn-northwest-1.amazonaws.com.cn/444603803904/ph-dagtrigger-pipeline.fifo",
         #     MessageBody=json.dumps(message, ensure_ascii=False),
         #     MessageGroupId=GenerateID().generate()
         # )
-
         response = self.sqs_client.send_message(
             QueueUrl="https://sqs.cn-northwest-1.amazonaws.com.cn/444603803904/ph-dsevent-pipeline.fifo",
             MessageBody=json.dumps(message, ensure_ascii=False),
@@ -35,4 +71,4 @@ class SQS():
             MessageBody=json.dumps(message, ensure_ascii=False),
             MessageGroupId=GenerateID().generate()
         )
-
+        '''

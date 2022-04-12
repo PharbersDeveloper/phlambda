6f153620827c4aec608239435dba2c273df0a3a8
AlfredYang1986 <yangyuanpig@gmail.com>
2022-04-12 19:18:25 +0800

    错误处理

--- phstatemachinefailedhook/src/main.py
+++ phstatemachinefailedhook/src/main.py
@@ -38,6 +38,26 @@ def put_notification(runnerId, projectId, category, code, comments, date, owner,
 #                                     1,
 #                                     'Count')
 
+def errorHandle(error, runnerId):
+    # 1. 看看是不是gen dag的错误，也就是说不能两个runnid 同时执行
+    # error = event['error']
+    print('=======> alfred test')
+    print(error)
+    if 'genDagError' in error and error['genDagError']['Error'] == 'AlreadyExistsException':
+        return error['genDagError']['Cause']        
+    
+    # 2. 看看是不是dag的错误
+    if 'executeError' in error:
+        executionArn = ':'.join(['arn:aws-cn:states:cn-northwest-1:444603803904:execution', runnerId, runnerId])
+        client = boto3.client('stepfunctions')
+        events = client.get_execution_history(executionArn=executionArn)['events']
+        eventsCount = len(events)
+        errorEvent = events[eventsCount - 1]
+        detail = json.loads(errorEvent['executionFailedEventDetails']['cause'])
+        print(detail)
+        return json.dumps(detail['Step']['Status'])
+
+    raise Exception('unknown')
 
 def lambda_handler(event, context):
     print(event)
@@ -55,41 +75,19 @@ def lambda_handler(event, context):
         KeyConditionExpression='id=:v1'
     )['Items']
 
-    # 1. 看看是不是gen dag的错误，也就是说不能两个runnid 同时执行
-    error = event['error']
-    if 'genDagError' in event['error'] and event['error']['genDagError']['Error'] == 'AlreadyExistsException':
-        message = ''
-        for item in items:
-            if item['projectId'] == event['projectId']:
-                message = event['error']['genDagError']['Cause']        
-                put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='failed', message=message)
-            else:
-                if item['status'] == 'running':
-                    put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='failed')
-                elif item['status'] == 'queued':
-                    put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='canceled')
-    
-    # 2. 看看是不是dag的错误
-    if 'executeError' in event['error']:
-        executionArn = ':'.join(['arn:aws-cn:states:cn-northwest-1:444603803904:execution', event['runnerId'], event['runnerId']])
-        client = boto3.client('stepfunctions')
-        events = client.get_execution_history(executionArn=executionArn)['events']
-        eventsCount = len(events)
-        errorEvent = events[eventsCount - 1]
-        detail = json.loads(errorEvent['executionFailedEventDetails']['cause'])
-        print(detail)
-        message = detail['Message']
-        jobName = detail['Name']
-
-        for item in items:
-            if item['projectId'] == event['projectId']:
-                put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='failed', message=message)
-            else:
-                if item['status'] == 'running':
-                    put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='failed')
-                elif item['status'] == 'queued':
-                    put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='canceled')
-
-
+    message = ''
+    try:
+        message = errorHandle(event['error'], event['runnerId'])
+    except:
+        message = 'unknown'
         
+    for item in items:
+        if item['projectId'] == event['projectId']:
+            put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='failed', message=message)
+        else:
+            if item['status'] == 'running':
+                put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='failed')
+            elif item['status'] == 'queued':
+                put_notification(item['id'], item['projectId'], None, 0, "", int(ts), event['owner'], event['showName'], status ='canceled')
+    
     return { "status": "ok"  }

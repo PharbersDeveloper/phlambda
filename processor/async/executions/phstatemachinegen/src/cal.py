4984990df2e30c3c7f363bc05f2f6b21f4004a74
AlfredYang1986 <yangyuanpig@gmail.com>
2022-04-28 19:44:27 +0800

    一个递归错误的bug

--- processor/async/executions/phstatemachinegen/src/cal.py
+++ processor/async/executions/phstatemachinegen/src/cal.py
@@ -44,32 +44,24 @@ def calDatasetPathOne(datasetName, datasets, jobs, links, stack):
     return True
 
 
-def calDatasetPath(datasetName, datasets, jobs, links, stack):
+def calDatasetPath(datasetName, datasets, jobs, links, stack, alljobs=[]):
     dslst = list(filter(lambda x: x['name'] == datasetName, datasets))
     if len(dslst) != 1:
         raise Exception('Wrong Arguments: datasetName')
         return datasetName
 
-    # curDs = dslst[0]
-    # print("============> alfredtest ds")
-    # print(curDs)
-    
-    # # if joblk next level dslk greater than 1, should out stack, donothing for inner stack
-    # curDslk = list(filter(lambda x: x['ll']['sourceName'] == curDs['name'], links))
-    # if len(curDslk) > 1:
-    #     return datasetName
-    # print("============> alfredtest dslk")
-    # print(curDslk)
-    # print(datasetName)
 
-
-    def calNextLevelJob(joblk, nlstack):
+    def calNextLevelJob(joblk, nlstack, alljobs):
         nldataset = list(filter(lambda x: x['name'] == joblk['ll']['targetName'], datasets))
         curDs = nldataset[0]
     
         # if joblk next level dslk greater than 1, should out stack, donothing for inner stack
-        curDslk = list(filter(lambda x: x['ll']['sourceName'] == curDs['name'], links))
+        tmplk = list(filter(lambda x: x['ll']['targetName'] in alljobs, links))
+        curDslk = list(filter(lambda x: x['ll']['sourceName'] == curDs['name'], tmplk))
         if len(curDslk) > 1:
+            print('alfredtest ============> 11111')
+            print(alljobs)
+            print(curDs['name'])
             return datasetName
 
         if len(nldataset) != 1:
@@ -94,8 +86,6 @@ def calDatasetPath(datasetName, datasets, jobs, links, stack):
             res.append(joblk[0]['ll']['sourceName'])
         
         result = list(set(res))
-        print('alfredtest =========> paralle')
-        print(result)
         
         if len(result) == 1:
             return result[0]
@@ -109,16 +99,18 @@ def calDatasetPath(datasetName, datasets, jobs, links, stack):
         # stack.append(llst[0])
         curJ = list(filter(lambda x: x['name'] == llst[0]['ll']['sourceName'], jobs))[0]
         stack.append(curJ)
+        alljobs.append(curJ['name'])
+        alljobs = list(set(alljobs))
         nldslk = list(filter(lambda x: x['ll']['targetName'] == llst[0]['ll']['sourceName'], links))
         nljoblk = dslk2joblk(nldslk, links)
         if len(nljoblk) == 1:
-            return calNextLevelJob(nljoblk[0], stack)
+            return calNextLevelJob(nljoblk[0], stack, alljobs)
         elif len(nljoblk) > 1:
             cstack = deque()
             tmpDsname = []
             for iter in nljoblk:
                 tstack = deque()
-                tmpRes = calNextLevelJob(iter, tstack)
+                tmpRes = calNextLevelJob(iter, tstack, alljobs)
                 if len(tstack) > 0:
                     tmpDsname.append(tmpRes)
                     cstack.append(tstack)
@@ -126,7 +118,7 @@ def calDatasetPath(datasetName, datasets, jobs, links, stack):
             # 并行完了还需要一层递归
             nldsname = calParalleCommonParent(tmpDsname)
             if len(nldsname) > 0:
-                return calDatasetPath(nldsname, datasets, jobs, links, stack)
+                return calDatasetPath(nldsname, datasets, jobs, links, stack, alljobs)
             
         else:
             # 递归退出逻辑

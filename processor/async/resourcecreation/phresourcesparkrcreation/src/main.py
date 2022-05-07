02119b3547336e3db7fb5b3340fe9feae1a8f566
Alex <891366402@qq.com>
2022-05-07 11:32:11 +0800

    创建文件时出错抛出异常

--- processor/async/resourcecreation/phresourcesparkrcreation/src/main.py
+++ processor/async/resourcecreation/phresourcesparkrcreation/src/main.py
@@ -32,39 +32,43 @@ def lambda_handler(event, context):
 
     # 创建R代码的流程
 
-    phs3 = PhS3()
-    name = f"{event['projectName']}_{event['dagName']}_{event['scripts']['flowVersion']}"
+    try:
+        phs3 = PhS3()
+        name = f"{event['projectName']}_{event['dagName']}_{event['scripts']['flowVersion']}"
 
-    # 1 收集参数，转成想要的结构
-    conf = {
-        "s3": phs3,
+        # 1 收集参数，转成想要的结构
+        conf = {
+            "s3": phs3,
 
-        "bucket": os.environ["BUCKET"],
-        "cliVersion": os.environ["CLI_VERSION"],
-        "jobPathPrefix": os.environ["JOB_PATH_PREFIX"],
-        "templatePhmainFile": os.environ["TM_PHMAIN_FILE"],
-        "templatePhjobFile": os.environ["TM_PHJOB_FILE"],
-        "dagS3JobsPath": os.environ["DAG_S3_JOBS_PATH"],
+            "bucket": os.environ["BUCKET"],
+            "cliVersion": os.environ["CLI_VERSION"],
+            "jobPathPrefix": os.environ["JOB_PATH_PREFIX"],
+            "templatePhmainFile": os.environ["TM_PHMAIN_FILE"],
+            "templatePhjobFile": os.environ["TM_PHJOB_FILE"],
+            "dagS3JobsPath": os.environ["DAG_S3_JOBS_PATH"],
 
-        "projectId": event["projectId"],
-        "projectName": event["projectName"],
-        "dagName": event["dagName"],
-        "flowVersion": event["scripts"]["flowVersion"],
-        "jobDisplayName": event["jobDisplayName"],
-        "inputs": json.loads(event["scripts"]["inputs"]),
-        "output": json.loads(event["scripts"]["output"])["name"],
-        "name": name,
-        "jobPath": os.environ["JOB_PATH_PREFIX"] + name + "/" + event["jobDisplayName"]
-    }
+            "traceId": event["traceId"],
+            "projectId": event["projectId"],
+            "projectName": event["projectName"],
+            "dagName": event["dagName"],
+            "flowVersion": event["scripts"]["flowVersion"],
+            "jobDisplayName": event["jobDisplayName"],
+            "inputs": json.loads(event["scripts"]["inputs"]),
+            "output": json.loads(event["scripts"]["output"])["name"],
+            "name": name,
+            "jobPath": os.environ["JOB_PATH_PREFIX"] + name + "/" + event["jobDisplayName"]
+        }
 
-    os.system("rm -rf " + conf.get("jobPath") + "/*")
-    subprocess.call(["mkdir", "-p", conf.get("jobPath")])
+        os.system("rm -rf " + conf.get("jobPath") + "/*")
+        subprocess.call(["mkdir", "-p", conf.get("jobPath")])
 
-    # 2 生成ph_main.R文件
-    create_ph_main_file(conf)
-    # 3 生成ph_job.R文件
-    create_ph_job_file(conf)
-    # 4 上传到S3对应的位置
-    upload_files(conf)
+        # 2 生成ph_main.R文件
+        create_ph_main_file(conf)
+        # 3 生成ph_job.R文件
+        create_ph_job_file(conf)
+        # 4 上传到S3对应的位置
+        upload_files(conf)
 
-    return {}
+        return True
+    except Exception as e:
+        raise Exception(f"creation sparkr file error, detail: {str(e)}")

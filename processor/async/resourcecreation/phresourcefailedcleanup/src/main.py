176341cf1472015ccb6c530988343881af157e91
AlfredYang1986 <yangyuanpig@gmail.com>
2022-05-07 18:19:02 +0800

    小修改

--- processor/async/resourcecreation/phresourcefailedcleanup/src/main.py
+++ processor/async/resourcecreation/phresourcefailedcleanup/src/main.py
@@ -131,7 +131,7 @@ def lambda_handler(event, context):
     # 1. 如果dataset name在dataset中存在，删除
     # 2. 如果scripts name在dataset中存在，删除
     # 3. 如果dag表中，ctype = node && represent-id 为上诉中的已经被删除的节点删除
-    # 4. 如果dag表中，ctype = node && cmessage 中 sourceId 或者 targetId 为上述中的删除节点的删除
+    # 4. 如果dag表中，ctype = link && cmessage 中 sourceId 或者 targetId 为上述中的删除节点的删除
     # 5. 删除s3中目标文件夹的文件
     #   5.1 每一个生成过程都给一个TraceID命名的文件，如果文件名一样，删除，如果文件不一样说明时别人创建的不能删除
 

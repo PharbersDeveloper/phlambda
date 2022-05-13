ca985c75c11c5325fe3862297976d32a033e5bb1
hbzhao <15847059648@163.com>
2022-05-13 14:07:06 +0800

    tenant

--- processor/async/tenantboot/phbootresourcemate/src/main.py
+++ processor/async/tenantboot/phbootresourcemate/src/main.py
@@ -1,5 +1,8 @@
 import json
+import boto3
+from boto3.dynamodb.conditions import Key
 
+dynamodb = boto3.resource('dynamodb')
 
 '''
 这个从resource表中读取需要创建的资源的matedata的描述
@@ -46,6 +49,19 @@ return = {
     }
 }
 '''
+
+
+def get_resource_items_by_tenantId(tenantId, role):
+    ds_table = dynamodb.Table('resource')
+    res = ds_table.query(
+        IndexName='resource-tenantId-role-index',
+        KeyConditionExpression=Key("tenantId").eq(tenantId)
+                               & Key("role").eq(role)
+    )
+
+    return res["Items"]
+
+
 def lambda_handler(event, context):
 
     # 从dynamodb 根据tenantId获取有关engine的信息

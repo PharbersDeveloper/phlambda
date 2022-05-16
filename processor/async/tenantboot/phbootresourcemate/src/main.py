9158b5a3d071de7bca38b1079d43a283d038290e
AlfredYang1986 <yangyuanpig@gmail.com>
2022-05-16 12:22:23 +0800

    meta 调试

--- processor/async/tenantboot/phbootresourcemate/src/main.py
+++ processor/async/tenantboot/phbootresourcemate/src/main.py
@@ -9,33 +9,36 @@ dynamodb = boto3.resource('dynamodb')
 也就是所从emr中读取emr的配
 
 args = {
-    "common": {
-        "traceId": "alfred-resource-creation-traceId",
-        "projectId": "ggjpDje0HUC2JW",
-        "projectName": "demo",
-        "owner": "alfred",
-        "showName": "alfred"
-    },
-    "resources": [
-        "engine", "olap"
-    ]
+    "tenantId.$": "$.common.tenantId",
+    "traceId.$": "$.common.traceId",
+    "projectId.$": "$.common.projectId",
+    "owner.$": "$.common.owner",
+    "showName.$": "$.common.showName",
+    "resources.$": "$.resources"
 }
 
 return = {
     "metadata": {
         "engines": [
             {   
-                "type": "ec2",
+                "type": "emr",
+                "version": "emr-6.2.0",
+                "label": [
+                    {
+                        "spark": "3.0.2",
+                        "hadoop": "3.2.0"
+                    }
+                ]
                 "cfn": "",
                 "parameters": {
                     "RootVolumeSize": 10,
                     "ReleaseLabel": "emr-6.2.0",
                     "MasterInstanceType": "m5.2xlarge",
                     "MasterStorage": 64,
-                    "CoreInstanceType": "m5.2xlarge"
+                    "CoreInstanceType": "m5.2xlarge",
                     "CoreStorage": 32,
                     "InitialCoreSize": 1,
-                    "MaxCoreSize": 2，
+                    "MaxCoreSize": 2,
                     "TaskInstanceType": "m5.2xlarge",
                     "TaskStorage": 32,
                     "TaskNodeOutThreshold": 10,
@@ -59,11 +62,6 @@ return = {
 }
 '''
 
-cfn_path = {
-    "clickhouse": "",
-    "emr": ""
-}
-
 
 def get_resource_items_by_tenantId(tenantId):
     ds_table = dynamodb.Table('resource')
@@ -74,40 +72,21 @@ def get_resource_items_by_tenantId(tenantId):
     return res["Items"]
 
 
-def create_engine_args(tenant_item):
-
-    args = tenant_item["properties"]
-    args["cfn"] = cfn_path[tenant_item["ctype"]]
-
-    return args
-
-
-def create_olap_args(tenant_item):
-
-    args = tenant_item["properties"]
-    args["cfn"] = cfn_path[tenant_item["ctype"]]
-
-    return args
-
-
 def lambda_handler(event, context):
-
+    print(event)
     # 获取有关tenantId的所有信息
     tenant_all_items = get_resource_items_by_tenantId(event["tenantId"])
+
     # 进行filter 查找
     metadata = {}
-    engine_metadata_list = []
-    olap_metadata_list = []
 
-    tenant_items = [item["ownership"] == "shared" for item in tenant_all_items]
+    # tenant_items = [item["ownership"] == "shared" for item in tenant_all_items]
+    tenant_items = list(filter(lambda x: x["ownership"] == "shared", tenant_all_items))
+    print(tenant_items)
+    # TODO: resources 里面的值与tenantItems 里面role值求交集，只有交集才能创建 @hbzhao
+
     for tenant_item in tenant_items:
-        # 从dynamodb 根据tenantId获取有关engine的信息
-        if tenant_item["role"] == "engine":
-            engine_metadata = create_engine_args(tenant_item)
-            engine_metadata_list.append(engine_metadata)
-        # 从dynamodb 根据tenantId获取有关olap的信息
-        if tenant_item["role"] == "olap":
-            olap_metadata = create_olap_args(tenant_item)
-            olap_metadata_list.append(olap_metadata)
+        metadata[tenant_item["role"]] = json.loads(tenant_item["properties"])
 
+    print(metadata)
     return metadata

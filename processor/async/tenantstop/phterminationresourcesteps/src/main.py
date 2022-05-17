4a05c4ca0458a70ed65b4c3a79afc8aec4a65a1a
alfred <yangyuanpig@gmail.com>
2022-05-17 19:00:55 +0800

    修改termination 流程

---
+++ processor/async/tenantstop/phterminationresourcesteps/src/main.py
@@ -0,0 +1,18 @@
+import json
+
+
+'''
+通过参数，删除所有的steps
+args = {
+    "traceId": "alfred-resource-creation-traceId",
+    "projectId": "ggjpDje0HUC2JW",
+    "projectName": "demo",
+    "owner": "alfred",
+    "showName": "alfred"
+    "resouces": "",
+    "role": ""
+}
+'''
+def lambda_handler(event, context):
+
+    return True

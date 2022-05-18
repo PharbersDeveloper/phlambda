a505b4eb95d6f35f02633a976af8d2391c939040
mzhang <zhangminwork@126.com>
2022-05-18 16:47:45 +0800

    三个lmd-- scenario,step,trigger的index 的删除，部分return形式对接时再做

--- processor/async/scenariosdeletion/phdelscenarioindex/src/main.py
+++ processor/async/scenariosdeletion/phdelscenarioindex/src/main.py
@@ -17,6 +17,43 @@ args:
     }
 '''
 
+class DelScenarioIndex:
+
+    def __init__(self, event):
+        self.event = event
+
+    def get_projectId(self):
+        return self.event['projectId']
+
+    def get_scenarioId(self):
+        return self.event['id']
+
+    def del_table_item(self, tableName, **kwargs):
+
+        DelItem = dict(kwargs.items())
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(tableName)
+        table.delete_item(
+            Key=DelItem,
+        )
+
+    #TODO 因为scenario为父级，所以需同时获取子级的OldImage,现在不知道返回的数据结果，后面对接的时候再做
+    def get_OldImage(self):
+        pass
+
+    def fetch_result(self):
+        return True
+
 
 def lambda_handler(event, context):
-    return True
+    DelClient = DelScenarioIndex(event)
+    #-------------------delete step---------------------------------------------#
+    #TODO 删除Item前需将数据保存到OldImage，以便后面回滚操作时用
+    DelClient.del_table_item('scenario_step', scenarioId=DelClient.get_scenarioId())
+    #-------------------delete trigger---------------------------------------------#
+    DelClient.del_table_item('scenario_trigger', scenarioId=DelClient.get_scenarioId())
+    #-------------------delete scenario---------------------------------------------#
+    DelClient.del_table_item('scenario', projectId=DelClient.get_projectId(), id=DelClient.get_scenarioId())
+    return DelClient.fetch_result()
+
+
--- processor/async/scenariosdeletion/phdelstepsindex/src/main.py
+++ processor/async/scenariosdeletion/phdelstepsindex/src/main.py
@@ -2,6 +2,7 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Attr,Key
 from datetime import datetime
+from decimal import Decimal
 
 '''
 删除steps里面的东西
@@ -18,6 +19,68 @@ args:
     }
 '''
 
+class DelStepsIndex:
+
+    def __init__(self, event):
+        self.event = event
+
+    def get_scenarioId(self):
+        return self.event['scenario']
+
+    def get_stepId(self):
+        return self.event['steps']
+
+    def query_table_item(self, tableName, **kwargs):
+        QueryItem = dict(kwargs.items())
+        dynamodb = boto3.resource('dynamodb')
+        ds_table = dynamodb.Table(tableName)
+        res = ds_table.query(
+           Key=QueryItem,
+        )
+        return res["Items"]
+
+
+    def del_table_item(self, tableName, **kwargs):
+
+        DelItem = dict(kwargs.items())
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(tableName)
+        table.delete_item(
+            Key=DelItem,
+        )
+
+    def turn_decimal_into_int(self, data):
+        return int(data) if isinstance(data, Decimal) else data
+
+    def get_OldImage(self, Items):
+        if len(Items) != 0:
+            ItemDict = Items[0]
+            OldImage = {
+                "confData": ItemDict['confData'],
+                "detail": ItemDict['detail'],
+                "index": self.turn_decimal_into_int(ItemDict['index']),
+                "mode": ItemDict['mode'],
+                "name": ItemDict['name'],
+                "id": ItemDict['id']
+            }
+        else:
+            OldImage = {}
+        self.OldImage = OldImage
+        return OldImage
+
+    def fetch_result(self):
+        return self.OldImage
+
 
 def lambda_handler(event, context):
-    return True
+    DelClient = DelStepsIndex(event)
+
+    #--------------------------get OldImage-------------------------------------------------------#
+    OldImageItem = DelClient.query_table_item('scenario_step', scenarioId=DelClient.get_scenarioId(), id=DelClient.get_stepId())
+    DelClient.get_OldImage(OldImageItem)
+
+    #-------------------------delete step----------------------------------------------------------#
+    DelClient.del_table_item('scenario_step', scenarioId=DelClient.get_scenarioId(), id=DelClient.get_stepId())
+
+    #TODO 返回结果后面对接时再调整
+    return DelClient.fetch_result()

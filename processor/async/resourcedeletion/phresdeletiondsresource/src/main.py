8195b394cd89da56f98f6f599d88bf0929c994b1
hbzhao <15847059648@163.com>
2022-05-18 10:41:53 +0800

    更改clusterId  clickhouse ip获取方式

--- processor/async/resourcedeletion/phresdeletiondsresource/src/main.py
+++ processor/async/resourcedeletion/phresdeletiondsresource/src/main.py
@@ -35,10 +35,10 @@ def del_s3_job_dir(bucket_name, s3_dir):
 
 
 def __create_clickhouse(resource):
-    proxies = resource.get("proxies")
+    proxies = resource.get("olap")
     ip = os.environ["CLICKHOUSE_HOST"]
     if len(proxies) > 0:
-        ip = proxies[0]
+        ip = proxies.get("PrivateIp")
     return ClickHouse(host=ip, port=os.environ["CLICKHOUSE_PORT"])
 
 

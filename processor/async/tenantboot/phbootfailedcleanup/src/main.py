b63287be799883be73efd03a04a1dcf9d103acc2
ylzhang <2824439675@qq.com>
2022-05-18 09:52:19 +0800

    tenantboot

--- processor/async/tenantboot/phbootfailedcleanup/src/main.py
+++ processor/async/tenantboot/phbootfailedcleanup/src/main.py
@@ -1,5 +1,5 @@
 import json
-
+import boto3
 
 '''
 同步错误，清理 cloudformation 以及 SSM 资源
@@ -27,6 +27,34 @@ args = {
     }
 }
 '''
-def lambda_handler(event, context):
 
-    return True
+
+class CleanUp:
+    cloudformation = boto3.client('cloudformation', region_name="cn-northwest-1",
+                                  aws_access_key_id="AKIAWPBDTVEANKEW2XNC",
+                                  aws_secret_access_key="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")
+
+    def del_stack(self, stackname):
+        response = self.cloudformation.delete_stack(
+            StackName=stackname,
+        )
+
+    def run(self, traceId, **kwargs):
+        self.del_stack(traceId)
+
+
+
+def lambda_handler(event, context):
+    common = event.get("common")
+    errors = event.get("errors")
+    CleanUp().run(**common)
+
+    return {
+        "type": "notification",
+        "opname": event["owner"],
+        "cnotification": {
+            "data": {},
+            "error": errors
+        }
+    }
+

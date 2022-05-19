63e3a139b1d9b183139dedf2d8bc3018e5316bf8
mzhang <zhangminwork@126.com>
2022-05-19 14:58:46 +0800

    lmd -- deltriggerresource trigger Rule的删除

--- processor/async/scenariosdeletion/phdeltriggerresource/src/main.py
+++ processor/async/scenariosdeletion/phdeltriggerresource/src/main.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-from boto3.dynamodb.conditions import Attr,Key
+from boto3.dynamodb.conditions import Attr, Key
 from datetime import datetime
 
 '''
@@ -18,6 +18,44 @@ args:
     }
 '''
 
+class DelTriggerRule:
+
+    def __int__(self, event):
+        self.event = event
+        self.result = {}
+
+    def get_projectId(self):
+
+        return self.event['projectId']
+
+    def get_scenarioId(self):
+
+        return self.event['scenario']
+
+    def get_triggerId(self):
+
+        return self.event['triggers']
+
+    def get_stackName(self):
+
+        return "-".join(["scenario", self.get_projectId(), self.get_scenarioId(), self.get_triggerId()])
+
+    def del_trigger_rule(self, stackName):
+        try:
+            client = boto3.client('cloudformation')
+            response = client.delete_stack(
+                StackName=stackName # event['runnerId']
+            )
+            self.result['status'] = 'ok'
+            self.result['message'] = f'delete {stackName} resource success'
+        except Exception as e:
+            self.result['status'] = 'error'
+            self.result['message'] = str(e)
+
 
 def lambda_handler(event, context):
-    return True
+
+    delRuleClient = DelTriggerRule(event)
+    delRuleClient.del_trigger_rule(delRuleClient.get_stackName())
+
+    return delRuleClient.result

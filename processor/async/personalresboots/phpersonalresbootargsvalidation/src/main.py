15c0f377eecec3e9b785bb0696989fdcb4c099b2
ylzhang <2824439675@qq.com>
2022-05-20 10:28:07 +0800

    phersonal

--- processor/async/personalresboots/phpersonalresbootargsvalidation/src/main.py
+++ processor/async/personalresboots/phpersonalresbootargsvalidation/src/main.py
@@ -1,5 +1,5 @@
 import json
-
+import boto3
 
 '''
 这个函数是对所有的reboot的数据参数的validate
@@ -24,10 +24,34 @@ args = {
     }
 }
 '''
+
+
+def get_ssm():
+    ssm = boto3.client('ssm', region_name="cn-northwest-1")
+    responses = ssm.describe_parameters().get("Parameters")
+    return [response.get("name") for response in responses]
+
+
+def check_parameter(data, **kwargs):
+    common = data.get("action", {})
+    # 1. action.cat 只能是 personalResBoots
+    if not common.get("cat") == "personalResBoots":
+        raise Exception('action.cat must be personalResBoots')
+
+    # 2. resourcesId 必须存在
+    if not data.get("resourcesId"):
+        raise Exception('resourcesId not exits')
+
+    # 3. ssm 中必须存在 key 为 tenantId的项
+    traceId = common.get("traceId")
+    if not traceId in get_ssm():
+        raise Exception('traceId not in ssm')
+
+    return True
+
+
 def lambda_handler(event, context):
-    
+    return check_parameter(**event)
     # 1. action.cat 只能是 personalResBoots
     # 2. resourcesId 必须存在
     # 3. ssm 中必须存在 key 为 tenantId的项
-
-    return True

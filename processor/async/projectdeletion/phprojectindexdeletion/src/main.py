e5fab1634b74e94c966621d51d0d6ec5ebd14e8d
mzhang <zhangminwork@126.com>
2022-05-31 13:40:21 +0800

    lmd -- phprojectindexdeletion 索引删除做完

--- processor/async/projectdeletion/phprojectindexdeletion/src/main.py
+++ processor/async/projectdeletion/phprojectindexdeletion/src/main.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
-
+from decimal import Decimal
 
 '''
 删除索引
@@ -32,8 +32,61 @@ args = {
 }
 '''
 
+_IndexTables = ['scenario', 'Datasets',  'dagconf', 'dag', 'action', 'notification', 'dashboard', 'exectuion', 'executionStatus', 'logs', 'scenario_trigger', 'scenario_step', 'step', 'slide', 'version']
+
+def query_table_item(tableName, QueryKey, Queryvalue):
+    dynamodb = boto3.resource('dynamodb')
+    ds_table = dynamodb.Table(tableName)
+    res = ds_table.query(
+        KeyConditionExpression=Key(QueryKey).eq(Queryvalue)
+    )
+    return res["Items"]
+
+def del_table_item(tableName, **kwargs):
+    dynamodb = boto3.resource('dynamodb')
+    delItem = dict(kwargs.items())
+    table = dynamodb.Table(tableName)
+    table.delete_item(
+        Key=delItem,
+    )
+
+def cause_decimal_to_int(dataOfDict):
+    keys = list(dataOfDict.keys())
+    for key in keys:
+        if isinstance(dataOfDict[key], Decimal):
+            dataOfDict[Key] = int(dataOfDict[Key])
+    return dataOfDict
+
+
+#----------tableName需小写--------------------------#
+IndexTables = [str(x).lower() for x in _IndexTables]
 
 def lambda_handler(event, context):
-    return True
+
+    projectId = event['projectId']
+
+    result = {}
+    try:
+        count = 0
+        sum_of_tables = len(IndexTables)
+        #------------------获取表中含index的Items数据--------------------------------------#
+        for table in IndexTables:
+            ItemsOfTable = query_table_item(table, 'projectId', projectId)
+            currentNum = count + 1
+            print(f"正在删除第{str(str(currentNum))}张表数据: {table}, 还剩{str(sum_of_tables-currentNum)}张表")
+            #-------------删除表中Item-------------------------------------#
+            for item in ItemsOfTable:
+                item = cause_decimal_to_int(item)
+                del_table_item(table, **item)
+        result['status'] = 'ok'
+        result['message'] = 'delete success'
+    except Exception as e:
+        print('*'.join(['*'*50, 'ERORR', '*'*50]) +'\n' + str(e))
+        result['status'] = 'error'
+        result['message'] = str(e)
+    return result
+
+
+
 
 

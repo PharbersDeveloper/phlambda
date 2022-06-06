971d570326e79caebb92e725cf9640381843ecca
alfred <yangyuanpig@gmail.com>
2022-06-06 10:11:20 +0800

    jupyter 启停

---
+++ processor/sync/triggers/phjupyterstoptrigger/src/main.py
@@ -0,0 +1,72 @@
+import os
+import json
+import boto3
+import traceback
+from boto3.dynamodb.conditions import Key
+
+
+def lambda_handler(event, context):
+    event = json.loads(event["body"])
+    print(event)
+    result = {
+        "status": "",
+        "message": "",
+        "trace_id": "",
+        "resouceId": ""
+    }
+    # trace_id = ""
+    # edition = "" if os.getenv("EDITION") == "V2" else "-dev"
+    # traceId = event.get("common").get("traceId")
+
+    # TODO: 缺判断当前这个是否已经启动 @mzhang
+
+    # 1. event to args
+    args = {
+        "common": {
+            "tenantId": event["tenantId"],
+            "traceId": event["traceId"],
+            "projectId": "ggjpDje0HUC2JW",
+            "projectName": "demo",
+            "owner": event["owner"],
+            "showName": event["showName"]
+        },
+        "action": {
+            "cat": "tenant-boot",
+            "desc": "reboot project",
+            "comments": "something need to say",
+            "message": "something need to say",
+            "required": True
+        },
+        "resourcesId": "resourceId",
+        "notification": {
+            "required": True
+        }   
+    }
+
+    try:
+        trace_id = args["common"]["traceId"]
+        # state_machine_arn = os.environ["ARN"]
+        state_machine_arn = f"arn:aws-cn:states:cn-northwest-1:444603803904:stateMachine:personal-res-terminate"
+        client = boto3.client("stepfunctions")
+        res = client.start_execution(stateMachineArn=state_machine_arn + edition,
+                                     name=trace_id, input=json.dumps(args, ensure_ascii=False))
+        run_arn = res["executionArn"]
+        print("Started run %s. ARN is %s.", trace_id, run_arn)
+        result["status"] = "succeed"
+        result["message"] = "start run " + trace_id
+        result["trace_id"] = trace_id
+
+    except Exception:
+        result["status"] = "failed"
+        result["message"] = "Couldn't start run " + trace_id
+        result["trace_id"] = trace_id
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH,DELETE",
+        },
+        "body": json.dumps(result)
+    }

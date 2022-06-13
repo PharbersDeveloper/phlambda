9ac66f1d9b78d072f7367c1f8b587e974df6e803
ylzhang <2824439675@qq.com>
2022-06-13 17:33:41 +0800

    phscenariotimertrigger

--- processor/sync/triggers/phscenariotimertrigger/src/main.py
+++ processor/sync/triggers/phscenariotimertrigger/src/main.py
@@ -1,44 +1,52 @@
 
-
 import json
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+
+def query_scenario(ProjectId, ScenarioId, **kwargs):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('scenario')
+    res = table.query(
+        KeyConditionExpression=Key("projectId").eq(ProjectId) & Key('id').eq(ScenarioId),
+    )
+    return res["Items"]
 
 
 def lambda_handler(event, context):
-    event = json.loads(event["body"])
     print(event)
-    tenantId = event["tenantId"]
+    scenario = query_scenario(**event)
 
-    # args = {
-    #     "common": {
-    #         "tenantId": tenantId,
-    #         "traceId": event["traceId"],
-    #         "projectId": "ggjpDje0HUC2JW",
-    #         "projectName": "demo",
-    #         "owner": event["owner"],
-    #         "showName": event["showName"]
-    #     },
-    #     "action": {
-    #         "cat": "personalResBoots",
-    #         "desc": "reboot project",
-    #         "comments": "something need to say",
-    #         "message": "something need to say",
-    #         "required": True
-    #     },
-    #     "notification": {
-    #         "required": True
-    #     },
-    #     "scenario": {
-    #         "scenarioId": "ggjpDje0HUC2JW_46c4fd03c44344f68f6848c6eb7cdab8",
-    #         "runtime": "manual"
-    #     }
-    # }
+    args = {
+        "common": {
+            "tenantId": event["TriggerId"],
+            "traceId": scenario["traceId"],
+            "projectId": event["ProjectId"],
+            "projectName": scenario["projectName"],
+            "owner": scenario["owner"],
+            "showName": scenario["showName"]
+        },
+        "action": {
+            "cat": "scenarioTrigger",
+            "desc": "scenarioTrigger",
+            "comments": "",
+            "message": "",
+            "required": True
+        },
+        "notification": {
+            "required": True
+        },
+        "scenario": {
+            "scenarioId": event["ScenarioId"],
+            "runtime": "timer"
+        }
+    }
 
-    trace_id = event["common"]["traceId"]
+    trace_id = args["common"]["traceId"]
     state_machine_arn = f"arn:aws-cn:states:cn-northwest-1:444603803904:stateMachine:scenario-trigger"
     client = boto3.client("stepfunctions")
-    res = client.start_execution(stateMachineArn=state_machine_arn, #+ edition,
-                                 name=trace_id, input=json.dumps(event, ensure_ascii=False))
+    res = client.start_execution(stateMachineArn=state_machine_arn,
+                                 name=trace_id, input=json.dumps(args, ensure_ascii=False))
     run_arn = res["executionArn"]
     print("Started run %s. ARN is %s.", trace_id, run_arn)
 

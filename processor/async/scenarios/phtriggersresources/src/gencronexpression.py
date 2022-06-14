0f174002f36afaaf4fb38a79433dc37f70b6e911
mzhang <example.qq.com>
2022-06-14 16:31:22 +0800

    修改cron表达式

--- processor/async/scenarios/phtriggersresources/src/gencronexpression.py
+++ processor/async/scenarios/phtriggersresources/src/gencronexpression.py
@@ -17,9 +17,12 @@ class GenCronExpression:
 
     def get_cron_expression(self):
         map_TimeAndValue, indexOfPeriod  = self.get_base()
-        result = list(map(lambda x: x[-1] + "/1" if map_TimeAndValue.index(x) < indexOfPeriod else (x[-1] + f"/{self.period_value}" if map_TimeAndValue.index(x) == indexOfPeriod else x[-1]), map_TimeAndValue))
+        print(map_TimeAndValue)
+        result = list(map(lambda x: x[-1] if map_TimeAndValue.index(x) < indexOfPeriod else (f"*/{self.period_value}" if map_TimeAndValue.index(x) == indexOfPeriod else x[-1]), map_TimeAndValue))
         cron_data = list(reversed(result))
         #-------asw event rule cron 精度为分，没有秒-------------#
         cron_expression = "cron(" + " ".join(cron_data[1:5]) + " ? " + cron_data[-1] + ")"
         print(cron_expression)
         return cron_expression
+
+

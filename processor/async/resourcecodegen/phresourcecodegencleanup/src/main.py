56d7ce2bc0a48f315267347ebae85bc9fe6fe9a5
ylzhang <2824439675@qq.com>
2022-06-16 15:14:46 +0800

    修改codegen step中expressions数据格式

--- processor/async/resourcecodegen/phresourcecodegencleanup/src/main.py
+++ processor/async/resourcecodegen/phresourcecodegencleanup/src/main.py
@@ -66,14 +66,14 @@ class CleanUp:
             }
         )
 
-    def run(self, steps, oldImage, **kwargs):
+    def run(self, steps, oldImage, ifsteps, **kwargs):
         for step in steps:
             pjName = step.get("pjName")
             stepId = step.get("stepId")
             self.del_item(pjName, stepId)
-
-        for step in oldImage:
-            self.put_item(step)
+        if ifsteps:
+            for step in oldImage:
+                self.put_item(step)
 
 
 def lambda_handler(event, context):

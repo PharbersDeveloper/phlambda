cbecfddd83577e5525d21079a0cb755bb5201376
hbzhao <15847059648@163.com>
2022-06-22 14:21:04 +0800

    asd

--- processor/sync/utils/phgetjupyternumber/src/main.py
+++ processor/sync/utils/phgetjupyternumber/src/main.py
@@ -3,9 +3,8 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
 
-dynamodb = boto3.resource("dynamodb", region_name="cn-northwest-1",
-                          aws_access_key_id="AKIAWPBDTVEANKEW2XNC",
-                          aws_secret_access_key="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")
+dynamodb = boto3.resource("dynamodb")
+
 
 def jupyterResourceCount(tenantId):
     result = dynamodb.Table("resource")
@@ -30,13 +29,15 @@ def lambda_handler(event, context):
     print(tenantId)
     try:
         if jupyterResourceCount(tenantId) <= 100:
-            priority = max([resource_item.get('priority', 0) for resource_item in query_resource(tenantId)])
+            numbers = [i+1 for i in range(100)]
+            prioritys = [int(resource_item.get('priority', 0)) for resource_item in query_resource(tenantId)]
+            priority = min(list(set(numbers) - set(prioritys))) if prioritys else 0
             print(priority)
-            result = {"status": 1, "priority": int(priority)}
+            result = {"code": 0, "message": "", "priority": priority}
         else:
             raise Exception('jupyter count greater than 100')
     except Exception as e:
-        result = {"status": 0, "priority": str(e)}
+        result = {"code": 1, "message": str(e), "priority": ""}
     finally:
         return {
             "statusCode": 200,

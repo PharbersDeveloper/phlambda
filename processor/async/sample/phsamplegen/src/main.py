af46e3a0c82bd2b7721087738216dbe04708b8cf
hbzhao <15847059648@163.com>
2022-07-01 16:40:57 +0800

    sample gen

--- processor/async/sample/phsamplegen/src/main.py
+++ processor/async/sample/phsamplegen/src/main.py
@@ -3,10 +3,10 @@ import json
 import boto3
 from datetime import datetime
 from collections import deque
-from cal import calDatasetPath, calDatasetPathOne
+# from cal import calDatasetPath, calDatasetPathOne
 from sample import create_sample_args
-from args import *
-from sms import *
+# from args import *
+# from sms import *
 dynamodb = boto3.resource('dynamodb')
 
 def put_notification(runnerId, projectId, category, code, comments, date, owner, showName,

de6b3b7e2750bf5ea343c7a107c5f6230c70967e
hbzhao <15847059648@163.com>
2022-07-07 15:12:26 +0800

    cicdclanup

--- devops/cicd/phcicdcleanup/src/main.py
+++ devops/cicd/phcicdcleanup/src/main.py
@@ -44,5 +44,21 @@ def del_s3_resource(bucket, key):
 def lambda_handler(event, context):
     print(event)
     # 如果创建失败 则删除cloudformation 同时删除s3上cicd/prefix/manage.yaml
-    # 如果update失败 将cicd/prefix/manage_back.yaml 文件恢复到manage.yaml文件1
+    # 判断processor 和trigger 里面的required是否为true
+    if event["processor"]["required"]:
+        del_s3_resource("ph-platform", "2020-11-11/cicd/" + event["processor"]["prefix"] + "/manage.yaml")
+        # 判断这次操作是不是update操作
+        if event["processorMetadata"]["changeSetMsg"]["changeSetName"]:
+            # 如果update失败 将cicd/prefix/manage_back.yaml 文件恢复到manage.yaml文件
+            copy_manage_resource("ph-platform", "2020-11-11/cicd/" + event["processor"]["prefix"])
+            del_s3_resource("ph-platform", "2020-11-11/cicd/" + event["processor"]["prefix"] + "/manage_back.yaml")
+    if event["trigger"]["required"]:
+        del_s3_resource("ph-platform", "2020-11-11/cicd/" + event["trigger"]["prefix"] + "/manage.yaml")
+        # 判断这次操作是不是update操作
+        if event["triggerMetadata"]["changeSetMsg"]["changeSetName"]:
+            # 如果update失败 将cicd/prefix/manage_back.yaml 文件恢复到manage.yaml文件 删除manage_back文件
+            copy_manage_resource("ph-platform", "2020-11-11/cicd/" + event["trigger"]["prefix"])
+            del_s3_resource("ph-platform", "2020-11-11/cicd/" + event["trigger"]["prefix"] + "/manage_back.yaml")
+
+
     return 1

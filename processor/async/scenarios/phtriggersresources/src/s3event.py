751b9689586f497e1713581a81186858234982ab
ylzhang <2824439675@qq.com>
2022-07-08 17:32:13 +0800

    scenario

--- processor/async/scenarios/phtriggersresources/src/s3event.py
+++ processor/async/scenarios/phtriggersresources/src/s3event.py
@@ -6,24 +6,23 @@ import time
 s3 = boto3.resource('s3')
 
 
-def bucket_notif_args(projectId, dsNames, **kwargs):
-    return list(map(lambda ds_name : {
-            'Id': f'{projectId}{ds_name}',
-            # 'Id': f'test',
-            'TopicArn': 'arn:aws-cn:sns:cn-northwest-1:444603803904:PH_NOTICE_S3',
-            'Events': ['s3:ObjectCreated:Put'],
-            'Filter': {'Key': {'FilterRules': [{
-                            'Name': 'prefix',
-                            'Value': f'2020-11-11/lake/pharbers/{projectId}/{ds_name}/'}]}}
-            }, dsNames))
-
 
 def s3_bucket_notification(**kwargs):
+    projectId = kwargs.get("projectId")
+    dsNames = kwargs.get("dsNames")
+    notif_args = list(map(lambda ds_name: {
+        'Id': f'{projectId}{ds_name}',
+        'TopicArn': 'arn:aws-cn:sns:cn-northwest-1:444603803904:PH_NOTICE_S3',
+        'Events': ['s3:ObjectCreated:Put'],
+        'Filter': {'Key': {'FilterRules': [{
+            'Name': 'prefix',
+            'Value': f'2020-11-11/lake/pharbers/{projectId}/{ds_name}/'}]}}
+                    }, dsNames))
+
     bucket_notification = s3.BucketNotification('ph-platform')
-    print(bucket_notif_args(**kwargs))
     response = bucket_notification.put(
         NotificationConfiguration={
-            'TopicConfigurations': bucket_notif_args(**kwargs)
+            'TopicConfigurations': notif_args
         },
         SkipDestinationValidation=True
     )

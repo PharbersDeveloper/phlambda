dc191a3ab864f2988edf56b08e78880091276c88
mzhang <example.qq.com>
2022-07-11 17:41:52 +0800

    share sm.json

--- processor/async/sample/phsamplegen/src/share.py
+++ processor/async/sample/phsamplegen/src/share.py
@@ -3,34 +3,15 @@ import json
 
 '''
 args = {
-    "common": {
-        "tranceId": "",
-        "runnerId": "sample_sample_developer_2022-07-04T05:49:39+00:00",
-        "projectId": "Dp10sMiAYXWxRZj",
-        "projectName": "sample",
-        "owner": "16dc4eb5-5ed3-4952-aaed-17b3cc5f638b",
-        "showName": "赵浩博",
-        "tenantId": "zudIcG_17yj8CEUoCTHg"
-    },
-    "shares": [
-        {
-            "target": "ds name", // 共享时的目标DS 名称
-            "targetCat": "catalog | intermediate | uploaded", // 共享时的目标DS的类型  catalog是数据目录  intermediate是结果数据集  uploaded 是上传的数据 都需要分别处理
-            "targetPartitionKeys": [
-                {
-                    "name": "key1",
-                    "type": "string"
-                },
-                {
-                    "name": "key2",
-                    "type": "string"
-                }
-            ],
-            "sourceSelectVersions": ["version1", "version2", "version3"]
-            "source": "ds name", // 共享时的源DS 名称
-        }
-    ]
-}
+        "runnerId.$": "$.common.runnerId",
+        "projectId.$": "$.common.projectId",
+        "projectName.$": "$.common.projectName",
+        "owner.$": "$.common.owner",
+        "showName.$": "$.common.showName",
+        "engine.$": "$.engine",
+        "calculate.$": "$.calculate",
+        "tenantId.$": "$.common.tenantId"
+      }
 '''
 
 
@@ -96,8 +77,10 @@ def create_share_args(event, ts):
             "projectId": event["projectId"],
             "projectName": event["projectName"],
             "owner": event["owner"],
-            "showName": event["showName"]
+            "showName": event["showName"],
+            "tenantId": event["tenantId"]
         },
+        "shares": conf.get("shares"),
         "compute_share": {
             "name": "compute_share",
             "type": "spark-submit",

fd111e76b535545eb75e43eb1f44a787fc89e172
hbzhao <15847059648@163.com>
2022-07-13 14:31:37 +0800

    template 文件修改

--- devops/cicd/phcicdcleanup/src/main.py
+++ devops/cicd/phcicdcleanup/src/main.py
@@ -106,12 +106,9 @@ def lambda_handler(event, context):
         functionName = event["utils"]["functionName"]
         if get_stack_status(functionName + "-apiresource"):
             # 如果update失败 将cicd/prefix/manage_back.yaml 文件恢复到manage.yaml文件 删除manage_back文件
-            copy_manage_resource("ph-platform", "2020-11-11/cicd/" + event["trigger"]["prefix"] + functionName)
+            copy_manage_resource("ph-platform", "2020-11-11/cicd/" + event["utils"]["prefix"] + functionName)
             # del_s3_resource("ph-platform", "2020-11-11/cicd/" + event["trigger"]["prefix"] + "/manage_back.yaml")
-        if judge_stack_exist(event["trigger"]["functionName"] + "codebuild"):
-            delete_stack(event["trigger"]["functionName"] + "codebuild")
-
-
-
+        if judge_stack_exist(event["utils"]["functionName"] + "codebuild"):
+            delete_stack(event["utils"]["functionName"] + "codebuild")
 
     return 1

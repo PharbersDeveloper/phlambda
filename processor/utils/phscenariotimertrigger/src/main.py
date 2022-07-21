501b5a695676597f0646f247c9a2811f87e59ec4
ylzhang <2824439675@qq.com>
2022-07-21 14:09:48 +0800

    添加程序保护 兼容scenario表空数据情况

--- processor/utils/phscenariotimertrigger/src/main.py
+++ processor/utils/phscenariotimertrigger/src/main.py
@@ -80,40 +80,40 @@ def lambda_handler(event, context):
             time.sleep(2)
 
     else:
-        scenario = query_scenario(**event)[0]
-
-        args = {
-            "common": {
-                "tenantId": event["TenantId"],
-                "traceId": traceId,
-                "projectId": event["ProjectId"],
-                "projectName": scenario["projectName"],
-                "owner": scenario["owner"],
-                "showName": scenario["showName"]
-            },
-            "action": {
-                "cat": "scenarioTrigger",
-                "desc": "scenarioTrigger",
-                "comments": "",
-                "message": "",
-                "required": True
-            },
-            "notification": {
-                "required": True
-            },
-            "scenario": {
-                "scenarioId": event["ScenarioId"],
-                "runtime": "timer"
+        # scenario = query_scenario(**event)[0]
+        for scenario in query_scenario(**event):
+            args = {
+                "common": {
+                    "tenantId": event["TenantId"],
+                    "traceId": traceId,
+                    "projectId": event["ProjectId"],
+                    "projectName": scenario.get("projectName"),
+                    "owner": scenario.get("owner"),
+                    "showName": scenario.get("showName")
+                },
+                "action": {
+                    "cat": "scenarioTrigger",
+                    "desc": "scenarioTrigger",
+                    "comments": "",
+                    "message": "",
+                    "required": True
+                },
+                "notification": {
+                    "required": True
+                },
+                "scenario": {
+                    "scenarioId": event["ScenarioId"],
+                    "runtime": "timer"
+                }
             }
-        }
 
-        trace_id = args["common"]["traceId"]
-        state_machine_arn = f"arn:aws-cn:states:cn-northwest-1:444603803904:stateMachine:scenariotrigger"
-        client = boto3.client("stepfunctions")
-        res = client.start_execution(stateMachineArn=state_machine_arn,
-                                     name=trace_id, input=json.dumps(args, ensure_ascii=False))
-        run_arn = res["executionArn"]
-        print("Started run %s. ARN is %s.", trace_id, run_arn)
+            trace_id = args["common"]["traceId"]
+            state_machine_arn = f"arn:aws-cn:states:cn-northwest-1:444603803904:stateMachine:scenariotrigger"
+            client = boto3.client("stepfunctions")
+            res = client.start_execution(stateMachineArn=state_machine_arn,
+                                         name=trace_id, input=json.dumps(args, ensure_ascii=False))
+            run_arn = res["executionArn"]
+            print("Started run %s. ARN is %s.", trace_id, run_arn)
 
     # ---------------------- 埋点 -------------------------------------#
     aws_cloudwatch_put_metric_data(NameSpace='pharbers-platform',

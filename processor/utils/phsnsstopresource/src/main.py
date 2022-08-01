8b8838cc5614988b10c2d5b66b21b7a0829a60eb
ylzhang <2824439675@qq.com>
2022-08-01 11:08:11 +0800

    定时删除资源 兼容c9

--- processor/utils/phsnsstopresource/src/main.py
+++ processor/utils/phsnsstopresource/src/main.py
@@ -2,13 +2,12 @@ import boto3
 from stopJupyter import StopJupyter
 from stopTenant import StopTenant
 
-
 COMMANDS = {
     'jupyter': StopJupyter,
 }
 
-
 cloudwatch = boto3.client("cloudwatch")
+
 
 def lambda_handler(event, context):
     # Records = event.get("Records", [])
@@ -16,13 +15,17 @@ def lambda_handler(event, context):
     #     MessageAttributes = record.get("Sns", {}).get("MessageAttributes", {})
     #     tenantId = MessageAttributes.get("tenantId", {}).get("Value", "")
     #     ctype = MessageAttributes.get("ctype", {}).get("Value", "")
+
     response = cloudwatch.describe_alarms(AlarmNamePrefix='platform-usage')
     tenantId = ''
-    for dimensions in [metricalarm.get("Dimensions") for metricalarm in response.get("MetricAlarms") if metricalarm.get("StateValue") == "ALARM"]:
+    for dimensions in [metricalarm.get("Dimensions") for metricalarm in response.get("MetricAlarms") if
+                       metricalarm.get("StateValue") == "ALARM"]:
         for dimen in dimensions:
             if dimen.get("Name") == 'tenantId':
                 tenantId = dimen.get("Value")
+
     if tenantId:
         StopJupyter().run(tenantId)
         StopTenant().run(tenantId)
+        StopJupyter().run(tenantId, ctype="c9")
     return True

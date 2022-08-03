4de4fbd95d1365e383f43800547ea13388b18c17
mzhang <example.qq.com>
2022-08-03 13:55:22 +0800

    sample error 代码已写完，目前错误类型仅EMR，后续出现新的类型再添加

--- processor/async/sample/phsampleerror/src/puttonotification.py
+++ processor/async/sample/phsampleerror/src/puttonotification.py
@@ -2,9 +2,8 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key
 
-def put_notification(runnerId, projectId, category, code, comments, date, owner, showName,
-                     jobCat='notification', jobDesc='executionSuccess', message='', status='queued',
-                     dynamodb=None):
+def put_notification(runnerId, projectId, category, code, comments, date, owner, showName, errorMessage,
+                     jobCat='notification', jobDesc='executionFailed', status='failed', dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb')
 
@@ -14,36 +13,28 @@ def put_notification(runnerId, projectId, category, code, comments, date, owner,
         "projectId": projectId,
         "cnotification": {
             "data": "{}",
-            "error": "{}",
+            "error": errorMessage,
             "runId": runnerId
         }
     }
 
     table = dynamodb.Table('notification')
-    res = table.query(
-        KeyConditionExpression=Key("id").eq(runnerId)
-                               & Key("projectId").begins_with(projectId)
+
+    response = table.put_item(
+        Item={
+            'id': runnerId,
+            'projectId': projectId,
+            'showName': showName,
+            'status': status,
+            'jobDesc': jobDesc,
+            'comments': comments,
+            'message': json.dumps(message, ensure_ascii=False),
+            'jobCat': jobCat,
+            'date': date,
+            'code': code,
+            'category': category,
+            'owner': owner
+        }
     )
-    if len(res["Items"]) == 0:
-        response = table.put_item(
-            Item={
-                'id': runnerId,
-                'projectId': projectId,
-                'showName': showName,
-                'status': status,
-                'jobDesc': jobDesc,
-                'comments': comments,
-                'message': json.dumps(message, ensure_ascii=False),
-                'jobCat': jobCat,
-                'date': date,
-                'code': code,
-                'category': category,
-                'owner': owner
-            }
-        )
-    else:
-        item = res["Items"][0]
-        item.update({"status": status})
-        response = table.put_item(Item=item)
     return response
 

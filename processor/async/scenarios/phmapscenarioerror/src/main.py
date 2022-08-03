ef997f385d739649cb3dd3f95ea1acd7be3f7c1c
mzhang <example.qq.com>
2022-08-03 18:16:48 +0800

    scenario error 调试

--- processor/async/scenarios/phmapscenarioerror/src/main.py
+++ processor/async/scenarios/phmapscenarioerror/src/main.py
@@ -50,8 +50,8 @@ def MapErrorType(cause):
         tmp = serialization(Errors)
     else:
         #--- 错误详情，用于解析映射用 -----#
-        ErrorKeys = list(*map(lambda x: list(x.keys()), cause))
-        if "KeyError" in ErrorKeys:
+        #ErrorKeys = list(*map(lambda x: list(x.keys()), cause))
+        if "KeyError" in cause:
             tmp = serialization(ParameterError)
         #TODO 后续出现新的错误类型再添加
         else:
@@ -68,12 +68,14 @@ def lambda_handler(event, context):
     print("*"*50 + "传入的 ERROR 信息 " + "*"*50)
     print(error)
 
-    cause = SearchErrorType(error)
-
-
-    #---- 基于error信息映射pherrorlayer ----------#
-    #TODO 目前对事件错误信息掌握不全，需要多次测试后再编写映射逻辑
-    errorMessage = MapErrorType(cause)
+    try:
+        cause = SearchErrorType(error)
+        #---- 基于error信息映射pherrorlayer ----------#
+        #TODO 目前对事件错误信息掌握不全，需要多次测试后再编写映射逻辑
+        errorMessage = MapErrorType(cause)
+    except Exception as e:
+        print("*"*50 + "代码异常" + "*"*50, e)
+        errorMessage = serialization(Errors)
 
     result = {
         "type": "notification",
@@ -93,11 +95,4 @@ if __name__ == '__main__':
 
     event = {'traceId': '569abb059b0940a68f21f3erw24b060df77', 'owner': '35cca7e1-45d9-4a6e-80f6-09e5417feb33', 'showName': '朗轩齐', 'scenario': {'id': 'ggjpDje0HUC2JW_5c689d48746846958cb62582a3f46qwer083', 'active': True, 'scenarioName': '00000', 'deletion': False}, 'projectName': 'demo', 'triggers': [], 'error': {'Error': 'KeyError', 'Cause': '{"errorMessage": "\'index\'", "errorType": "KeyError", "stackTrace": ["  File \\"/var/task/main.py\\", line 119, in lambda_handler\\n    scenarioClient.put_item()\\n", "  File \\"/var/task/main.py\\", line 71, in put_item\\n    \'index\': self.turn_decimal_into_int(self.get_index()),\\n", "  File \\"/var/task/main.py\\", line 42, in get_index\\n    return self.scenario[\'index\']\\n"]}'}, 'projectId': 'ggjpDje0HeqrewUC2JW', 'steps': []}
     lambda_handler(event,"")
-
-
-
-
-
-
-
 

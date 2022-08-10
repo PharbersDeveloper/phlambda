2f67538b2b28c654ab66b289dede4c914d4e9865
hbzhao <15847059648@163.com>
2022-08-10 14:41:07 +0800

    jira号:3236 前端cicd 增加失效 待测试

--- devops/frontendcicd/phfrontendcicdcreateinvalidation/src/main.py
+++ devops/frontendcicd/phfrontendcicdcreateinvalidation/src/main.py
@@ -19,35 +19,39 @@ args:
 '''
 
 
-def create_cloudfront_invalidation(bucket, key):
+def create_cloudfront_invalidation(bucket, keys):
     response = cloudfront_client.list_distributions()
 
     items = response["DistributionList"]["Items"]
     for item in response["DistributionList"]["Items"]:
         for i in item["Origins"]["Items"]:
-            # print(i)
             if i.get("DomainName").startswith(bucket):
                 distribution_id = item.get("Id")
-
     print(distribution_id)
+
     cloudfront_client.create_invalidation(
         DistributionId=distribution_id,
         InvalidationBatch={
             'Paths': {
-                'Quantity': 2,
-                'Items': [
-                    "/index.html",
-                    "/robots.txt",
-                ]
+                'Quantity': len(keys),
+                'Items': keys
             },
             'CallerReference': str(math.floor(datetime.datetime.now().timestamp() * 1000))
         }
     )
 
+
 def lambda_handler(event, context):
     print(event)
     # 获取上传到s3的文件路径
-    deploy_s3_paths = event["deploy_s3_paths"]
-    for s3_path_map in deploy_s3_paths:
-        create_cloudfront_invalidation(bucket=s3_path_map["bucket"], key="/" + s3_path_map["key"])
+    if event["invalidate"]:
+        deploy_s3_paths = event["deployS3Paths"]
+        invalidations_maps = {}
+        buckets = list(set(map(lambda x: x["bucket"], deploy_s3_paths)))
+        for bucket in buckets:
+            invalidations_maps.update({bucket: []})
+        for deploy_s3_path in deploy_s3_paths:
+            invalidations_maps[deploy_s3_path["bucket"]].append("/" + deploy_s3_path["key"])
+        for bucket, keys in invalidations_maps.items():
+            create_cloudfront_invalidation(bucket=bucket, keys=keys)
     return 1

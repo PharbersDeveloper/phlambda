03a8f114a6f0b44c140e28314bf56cbea791c059
mzhang <example.qq.com>
2022-08-17 17:54:08 +0800

    解决代码冲突

--- processor/async/scenarios/phscenarioargsvalidation/src/main.py
+++ processor/async/scenarios/phscenarioargsvalidation/src/main.py
@@ -1,6 +1,7 @@
 import json
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
+from boto3.dynamodb.conditions import Attr, Key
+
 '''
 这个函数只做一件事情，检查参数是否合法
 args:
@@ -60,60 +61,62 @@ args:
     }
 '''
 
-dynamodb = boto3.resource("dynamodb")
-
-
-def query_scenario(projectId, scenarioName):
-    table = dynamodb.Table("scenario")
-    response = table.query(
-        IndexName='projectId-scenarioName-index',
-        KeyConditionExpression=Key('projectId').eq(projectId) & Key('scenarioName').eq(scenarioName)
-    )
-    return response.get("Items")
-
-
 class CheckParameters:
+
     def __init__(self, event):
         self.event = event
+        #------ 参数类型 -------#
         self.check_dict = {
-            "common": self.check_common,
-            "action": self.check_action,
-            "notification": self.check_notificaton,
-            "scenario": self.check_scenario,
-            "triggers": self.check_triggers,
-            "steps": self.check_steps,
+            "common": dict,
+            "action": dict,
+            "notification": dict,
+            "scenario": dict,
+            "triggers": list,
+            "steps": list,
+            "reports": list
         }
+
+
     def get_prefix_key(self):
+
         return list(self.event.keys())
+
     def check_key(self, key):
-        return self.check_dict[key](key)
-    def check_type(self,key,type):
+
+        InputParameterType = self.check_dict[key]
+
+        return self.Check_Parameter_Type(key, InputParameterType)
+
+    def Check_Parameter_Type(self, key, InputParameterType):
         try:
-            if isinstance(self.event[key], type) is False:
-                raise Exception(f"type error: {key} is not belong {str(type)}")
+            if isinstance(self.event[key], InputParameterType) is False:
+                raise Exception(f"type error: {key} is not belong {str(InputParameterType)}")
         except Exception as e:
             raise e
-    def check_common(self, key):
-        self.check_type(key, dict)
-    def check_action(self, key):
-        self.check_type(key, dict)
-    def check_notificaton(self, key):
-        self.check_type(key, dict)
-    def check_scenario(self, key):
-        self.check_type(key, dict)
-    def check_triggers(self, key):
-        self.check_type(key, list)
-    def check_steps(self, key):
-        self.check_type(key, list)
+
+
 class Check:
+
+    def query_scenario(self, projectId, scenarioName):
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table("scenario")
+        response = table.query(
+            IndexName='projectId-scenarioName-index',
+            KeyConditionExpression=Key('projectId').eq(projectId) & Key('scenarioName').eq(scenarioName)
+        )
+        return response.get("Items")
+
+
     def check_parameter(self, event):
         event_data = CheckParameters(event)
         input_keys = event_data.get_prefix_key()
         #---------------------------------检查字段缺失---------------------------------------------------------#
         _key_triggers = ['common', 'action', 'notification', 'scenario', 'triggers']
         _key_steps = ['common', 'action', 'notification', 'scenario',  'steps']
-        _key_all = ['common', 'action', 'notification', 'scenario', 'triggers', 'steps']
-        if any((set(input_keys) >= set(_key_triggers), set(input_keys)>=set(_key_steps), set(input_keys)>=set(_key_all))):
+        _key_reports = ['common', 'action', 'notification', 'scenario',  'reports']
+        _key_all = ['common', 'action', 'notification', 'scenario', 'triggers', 'steps', 'reports']
+        if any((set(input_keys) >= set(_key_triggers), set(input_keys) >= set(_key_steps),
+                set(input_keys) >= set(_key_reports), set(input_keys) >= set(_key_all))):
             for key in input_keys:
                 if key in _key_all:
                     #----检查内层每个字段------#
@@ -122,12 +125,16 @@ class Check:
             missing_field = " or ".join([x for x in _key_all if x not in input_keys])
             raise Exception(f"Field missing: {input_keys} possible missing fields {missing_field}")
 
-        for scenario in query_scenario(event.get("common").get("projectId"), event.get("scenario").get("scenarioName")):
+        #----- 张玉磊 ScenarioName 重复验证 ------#
+        for scenario in self.query_scenario(event.get("common").get("projectId"),
+                                            event.get("scenario").get("scenarioName")):
             if scenario["id"] != event.get("scenario").get("id"):
                 raise Exception(f"scenario name already exist")
+
         return True
 
 def lambda_handler(event, context):
+
     return Check().check_parameter(event)
 
 # 1. common 必须存在

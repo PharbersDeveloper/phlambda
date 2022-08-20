cffa9fd993f45b7e7a98be397dd3187819ede819
mzhang <example.qq.com>
2022-08-20 11:16:54 +0800

    邮件发送 execution  时间优化

--- processor/async/scenariotrigger/phscenariotriggerrecord/src/main.py
+++ processor/async/scenariotrigger/phscenariotriggerrecord/src/main.py
@@ -36,21 +36,50 @@ reporter
 '''
 
 
+def query_table_item(tableName, **kwargs):
+    dynamodb = boto3.resource('dynamodb')
+    ds_table = dynamodb.Table(tableName)
+    res = ds_table.get_item(
+        Key=kwargs,
+    )
+    try:
+        Item = res["Item"]
+    except:
+        Item = {}
+    return Item
+
+
+
 def lambda_handler(event, context):
 
     ds_table = dynamodb.Table('scenario_execution')
 
-    response = ds_table.put_item(
-        Item={
-            'scenarioId': event['scenarioId'],
-            'runnerId': event['runnerId'],
-            'date': math.floor(datetime.datetime.now().timestamp() * 1000),
-            'runtime': event['runtime'],
-            'owner': event['owner'],
-            'reporter': event['reporter'],
-            'traceId': event['traceId'],
-            'stepId': event['stepId']
-        }
-    )
+
+    #----------- 记录每个exectuion 执行的开始时间和结束时间 -------------#
+
+    item_of_execution = query_table_item('scenario_execution',
+                                         scenarioId=event['scenarioId'],
+                                         runnerId=event['runnerId'])
+
+    if len(item_of_execution) == 0:
+        #----- execution 开始 -------#
+        response = ds_table.put_item(
+            Item={
+                'scenarioId': event['scenarioId'],
+                'runnerId': event['runnerId'],
+                'date': math.floor(datetime.datetime.now().timestamp() * 1000),
+                'runtime': event['runtime'],
+                'owner': event['owner'],
+                'reporter': event['reporter'],
+                'traceId': event['traceId'],
+                'stepId': event['stepId']
+            }
+        )
+    else:
+        #----- execution 结束 -------#
+        item_of_execution['stopdate'] = math.floor(datetime.datetime.now().timestamp() * 1000),
+        response = ds_table.put_item(
+            Item=item_of_execution
+        )
 
     return True

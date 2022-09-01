6975ea8cc8c5efc22821309b1e05600fa21736b2
ylzhang <2824439675@qq.com>
2022-09-01 11:33:26 +0800

    jira号: 3392 fix: scenario 重构

--- processor/async/scenariocheckcalllback/phscenariocheck/src/main.py
+++ processor/async/scenariocheckcalllback/phscenariocheck/src/main.py
@@ -2,10 +2,17 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Attr, Key
+import time
+from datetime import datetime
 from decimal import Decimal
 
 
 dynamodb = boto3.resource("dynamodb")
+now_time = int(time.time())
+
+# def make_time(strtime):
+#     a = datetime.strptime(strtime, '%Y-%m-%d %H:%M:%S').timetuple()
+#     return int(time.mktime(a))
 
 
 def query_dag(projectId, name=None, **kwargs):
@@ -21,6 +28,15 @@ def query_dag(projectId, name=None, **kwargs):
             KeyConditionExpression=Key("projectId").eq(projectId),
         )
     return res["Items"]
+
+
+def put_item(items):
+    for item in items:
+        item["lastruntime"] = now_time
+        dynamodb_table = dynamodb.Table("scenario_trigger")
+        response = dynamodb_table.put_item(
+            Item=item
+        )
 
 
 def query_scenario(projectId):
@@ -55,6 +71,8 @@ def execution(projectId, name, **kwargs):
             names.append(targetName)
     for id in query_scenario(projectId):
         triggers = triggers + query_trigger(id, names)
+
+    put_item(triggers)
     return triggers
 
 
@@ -66,7 +84,26 @@ def upload_share(projectId, name):
 
 
 def timer():
-    pass
+    triggers = []
+    period = {
+        "minute": 60, "hour": 3600, "day": 86400, "week": 604800, "month": 2628000,
+    }
+    table = dynamodb.Table('scenario')
+    scenario_response = table.scan(
+        FilterExpression=Attr("mode").eq("dataset") & Attr("active").eq(True)
+    )
+    scenario_ids = [scenario.get("id") for scenario in scenario_response.get("Items")]
+    table = dynamodb.Table('scenario_trigger')
+    for scenarioid in scenario_ids:
+        response = table.query(
+            KeyConditionExpression=Key('scenarioId').eq(scenarioid),
+            FilterExpression=Attr("mode").eq("timer")
+        ).get("Items")
+        triggers += [trigger for trigger in response if now_time >= int(trigger.get("lastruntime")) +
+                     period[json.loads(trigger.get("detail", {})).get("period")] *
+                     int(json.loads(trigger.get("detail", {})).get("value"))]
+
+    return triggers
 
 
 Command = {
